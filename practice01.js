2d066ab1fe6bee2f6c1cd12c99d667b39ac67654
seanaleid <seanaleid@gmail.com>
2020-04-27 21:29:19 -0400

    Add template for other problems

--- practice01.js
+++ practice01.js
@@ -1,6 +1,27 @@
 
+// Template
+// Date: 
+// Source: 
+// Title: 
+
+// Instructions:
+// 
+
+// Examples:
+// 
+
+// Notes:
+// 
+
+// Sudo code:
+// 1 - 
+
+// Work: 
+
+
 // Date: 27 - April - 2020
 // Source: https://edabit.com/challenge/4gzDuDkompAqujpRi
+// Title: Add up the Numbers from a Single Number
 
 // Instructions:
 // Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
