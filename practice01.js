f0fb19808ee8e9535304d5f59985dbf696ea2bca
seanaleid <seanaleid@gmail.com>
2020-05-03 20:28:11 -0400

    Find the smallest and biggest numbers

--- practice01.js
+++ practice01.js
@@ -21,6 +21,38 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: - May - 2020
+// Source: https://edabit.com/challenge/Q3n42rEWanZSTmsJm
+// Title: Find the Smallest and Biggest Numbers
+
+// Instructions:
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// Examples:
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+// minMax([2334454, 5]) ➞ [5, 2334454]
+// minMax([1]) ➞ [1, 1]
+
+// Notes:
+// All test arrays will have at least one element and are valid.
+
+// Sudo code:
+// 1 - 
+
+// Work: 
+
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)];
+}
+
+console.log(minMax([1, 2, 3, 4, 5]));
+console.log(minMax([2334454, 5]));
+console.log(minMax([1]));
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 02 - May - 2020
 // Source: https://edabit.com/challenge/pEzxi4MqHGrAi7ZdA
 // Title: Find the Index (Part 1)
 
@@ -39,7 +71,7 @@
 
 // function search(arr, item){
 //     // return arr.indexOf(item);
-  
+
 //     if (arr.includes(item)) {
 //         const result = arr.indexOf(item)
         
