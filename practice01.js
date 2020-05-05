8e74fe612ea304199fb984f09a6ffae517d5db19
seanaleid <seanaleid@gmail.com>
2020-05-05 13:24:56 -0400

    Check if an Array Contains a Given Number

--- practice01.js
+++ practice01.js
@@ -21,6 +21,42 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 05 - May - 2020
+// Source: Check if an Array Contains a Given Number
+// Title: https://edabit.com/challenge/SwyjHvkqwwQ2iJsoS
+
+// Instructions:
+// Write a function to check if an array contains a particular number.
+
+// Examples:
+// check([1, 2, 3, 4, 5], 3) ➞ true
+// check([1, 1, 2, 1, 1], 3) ➞ false
+// check([5, 5, 5, 6], 5) ➞ true
+// check([], 5) ➞ false
+
+// Notes:
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+
+// Work: 
+
+function check(arr, el){
+    if(arr.includes(el)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(check([1, 2, 3, 4, 5], 3));
+console.log(check([1, 1, 2, 1, 1], 3));
+console.log(check([5, 5, 5, 6], 5));
+console.log(check([], 5));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 05 - May - 2020
 // Source: https://edabit.com/challenge/nhXofMMyrowMyr9Nv
 // Title: Maximum Edge of a triangle
 
