1e9aa9b14841865619e40c61ff09db67b740e894
seanaleid <seanaleid@gmail.com>
2020-05-05 13:13:15 -0400

    Maximum edge of a triangle

--- practice01.js
+++ practice01.js
@@ -20,6 +20,34 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 05 - May - 2020
+// Source: https://edabit.com/challenge/nhXofMMyrowMyr9Nv
+// Title: Maximum Edge of a triangle
+
+// Instructions:
+// Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+
+// Examples:
+// nextEdge(8, 10) ➞ 17
+// nextEdge(5, 7) ➞ 11
+// nextEdge(9, 2) ➞ 10
+
+// Notes:
+// (side1 + side2) - 1 = maximum range of third edge.
+// The side lengths of the triangle are positive integers.
+// Don't forget to return the result.
+
+// Work: 
+// function nextEdge( side1, side2) {
+//     return (side1 + side2) - 1;
+// }
+
+// console.log(nextEdge(8, 10))
+// console.log(nextEdge(5, 7))
+// console.log(nextEdge(9, 2))
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 04 - May - 2020
 // Source: https://edabit.com/challenge/o7TwicAHWuMkjbDqQ
 // Title: Which Function Returns the Larger Number?
