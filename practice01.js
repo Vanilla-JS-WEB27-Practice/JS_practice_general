92c6a241a0c984b61a70edf4ab73686145bf2c60
seanaleid <seanaleid@gmail.com>
2020-05-11 18:09:23 -0400

    Convert a Number to Base 2

--- practice01.js
+++ practice01.js
@@ -20,6 +20,40 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: - May - 2020
+// Source: https://edabit.com/challenge/3kcrnpHk7krNZdnKK
+// Title: Convert a Number to Base 2
+
+// Instructions:
+// Create a function that returns a base 2 (binary) representation of a base 10 (decimal) string number. To convert is simple: ((2) means base 2 and (10) means base 10) 010101001(2) = 1 + 8 + 32 + 128.
+// Going from right to left, the value of the most right bit is 1, now from that every bit to the left will be x2 the value, value of an 8 bit binary numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
+
+// Examples:
+// binary(1) ➞ "1"
+// 1*1 = 1
+// binary(5) ➞ "101"
+// 1*1 + 1*4 = 5
+// binary(10) ➞ "1010"
+// 1*2 + 1*8 = 10
+
+// Notes:
+// Numbers will always be below 1024 (not including 1024).
+// The && operator could be useful.
+// The strings will always go to the length at which the most left bit's value gets bigger than the number in decimal.
+
+// Additional notes: parseInt() is the function that does this already. Takes two arguments and returns an integer in the specified radix (second argument, basically which base you want between 2 - 36). Binary is base 2, decimal is base 10. octal is base 8, hexadecimal is base 16 (hex codes)
+
+// Work: 
+// function binary(decimal){
+//     return parseInt(decimal, 10).toString(2);
+// }
+
+// console.log(binary(1));
+// console.log(binary(5));
+// console.log(binary(10));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 10 - May - 2020
 // Source: https://edabit.com/challenge/ms3q5GYSpFpwxeFWX
 // Title: Volume of a Box
