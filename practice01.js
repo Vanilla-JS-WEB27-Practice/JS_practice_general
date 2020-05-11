e725ec8356c113255856c64f773727e7633e2f2f
seanaleid <seanaleid@gmail.com>
2020-05-10 22:09:19 -0400

    Find the Index (Part 2)

--- practice01.js
+++ practice01.js
@@ -21,6 +21,33 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 10 - May - 2020
+// Source: https://edabit.com/challenge/z6Pxiw289JtaE2ndL
+// Title: FInd the Index (Part 2)
+
+// Instructions:
+// Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+
+// Examples:
+// search([1, 2, 3, 4], 3) ➞ 2
+// search([2, 4, 6, 8, 10], 8) ➞ 3
+// search([1, 3, 5, 7, 9], 11) ➞ -1
+
+// Notes:
+// If the item is not present, return -1.
+// The given array is ordered.
+
+// Work: 
+// function search(arr, item){
+//     return arr.indexOf(item);
+// }
+
+// console.log(search([1,2,3,4], 3));
+// console.log(search([2,4,6,8,10], 8));
+// console.log(search([1,3,5,7,9], 11));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 10 - May - 2020
 // Source: https://edabit.com/challenge/H25aG5aAdmFcMpBsg
 // Title: To the Power of _____
 
