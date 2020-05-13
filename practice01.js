7cdc7856ed51e085abae9ce6c215b8e7a7aaafc0
seanaleid <seanaleid@gmail.com>
2020-05-13 10:22:14 -0400

    Promises IV: The Reject Callback

--- practice01.js
+++ practice01.js
@@ -20,6 +20,41 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 13 - May - 2020
+// Source: https://edabit.com/challenge/HT2dHkbzmLDZgsS7G
+// Title: Promises IV: The Reject Callback
+
+// Instructions:
+// Async operations don't always go as planned. When errors creep up we need to know how to handle them. We can pass the reject callback to our executor function to pass errors to our promise.
+
+// Examples:
+// let promise = new Promise( (resolve, reject) => {
+//   setTimeout(( ) => {
+//     /* something went wrong */
+//      reject('oops!')
+//   }, 1000)
+// })
+
+// Notes:
+// You can pass Error objects as well. Here we pass a simple string "oops!".
+// Reject the promise with the simple string "It's not a dog!".
+// Check the Resources tab for more info on promises.
+
+// Work: 
+// let promise = new Promise( (resolve) => {
+// 	let animal = "cat"
+//   setTimeout(() => {
+// 		if(animal === "dog") {
+// 			resolve("It's a dog!")
+// 		} 
+// 	  if(animal !== "dog") {
+// 			reject( "It's not a dog!")
+// 		}
+//   }, 1000)
+// })
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 12 - May - 2020
 // Source: https://edabit.com/challenge/kbFhwaDyrd79JrgeB
 // Title: Count Instances of a Character in a String
