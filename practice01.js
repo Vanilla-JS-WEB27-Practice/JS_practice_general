554b2cb3dcedb594959d10ab8f1ac1921e6e50bb
seanaleid <seanaleid@gmail.com>
2020-05-15 17:57:49 -0400

    Filter out Strings from an Array

--- practice01.js
+++ practice01.js
@@ -20,6 +20,39 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: - May - 2020
+// Source: https://edabit.com/challenge/b2NdDSdkjqFnCTfS8
+// Title: Filter out Strings from an Array
+
+// Instructions:
+// Create a function that takes an array of non-negative integers and strings and return a new array without the strings.
+
+// Examples:
+// filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+// filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+// filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+
+// Notes:
+// Zero is a non-negative integer.
+
+// Work: 
+function filterArray(arr){
+    const newArr = arr.filter(str => {
+        if (str >= 0){
+            return Number.isInteger(str);
+        } 
+        return null;
+    })
+
+    return newArr;
+}
+
+console.log(filterArray([1, 2, "a", "b"]));
+console.log(filterArray([1, "a", "b", 0, 15]));
+console.log(filterArray([1, 2, "aasf", "1", "123", 123]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 14 - May - 2020
 // Source: https://edabit.com/challenge/Gpy2qSFnfhGJnWMMj
 // Title: Check if One Array can be Nested in Another
