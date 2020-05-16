d748350aaa29811f6f7968b84627421cf5c36210
seanaleid <seanaleid@gmail.com>
2020-05-16 01:26:41 -0400

    Return the Next Number from the Integer Passed

--- practice01.py
+++ practice01.py
@@ -17,11 +17,61 @@
 # 1 - 
 
 # Work: 
+
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 16 - May - 2020
+# Source: https://edabit.com/challenge/KjCS7occ9hfu5snpb
+# Title: Return the Next Number from the Integer Passed
+
+# Instructions:
+# Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+# Examples:
+# addition(0) ➞ 1
+# addition(9) ➞ 10
+# addition(-3) ➞ -2
+
+# Notes:
+# Don't forget to return the result.
+# If you get stuck on a challenge, find help in the Resources tab.
+# If you're really stuck, unlock solutions in the Solutions tab.
+
+# Work: 
+# def addition(num):
+#     return num + 1
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 16 - May - 2020
+# Source: https://edabit.com/challenge/aWLTzrRsrw7RakYrN
+# Title: Area of a Triangle
+
+# Instructions:
+# Write a function that takes the base and height of a triangle and return its area.
+
+# Examples:
+# tri_area(3, 2) ➞ 3
+# tri_area(7, 4) ➞ 14
+# tri_area(10, 10) ➞ 50
+
+# Notes:
+# The area of a triangle is: (base * height) / 2
+# Don't forget to return the result.
+# If you get stuck on a challenge, find help in the Resources tab.
+# If you're really stuck, unlock solutions in the Solutions tab.
+
+# Work: 
+# def tri_area(base, height):
+#     return (base * height)/2
+
+# ------------------------------------------------------------------------------------------------------------------
+
+## FIRST PYHTON EXPRESSION!!
+# Date: 16 - May - 2020
 # Source: https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
 # Title: Return the Sum of Two Numbers
+# Resources: https://learn.lambdaschool.com/cs/module/recay2erzDlYUPSeO/
 
 # Instructions:
 # Create a function that takes two numbers as arguments and return their sum.
