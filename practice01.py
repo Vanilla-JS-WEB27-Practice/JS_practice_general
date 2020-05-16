be7369aaf948389c24fd21121f50cfc7e12556a7
seanaleid <seanaleid@gmail.com>
2020-05-16 01:22:51 -0400

    Python exercises

---
+++ practice01.py
@@ -0,0 +1,44 @@
+
+# Template
+# Date: - May - 2020
+# Source: 
+# Title: 
+
+# Instructions:
+# 
+
+# Examples:
+# 
+
+# Notes:
+# 
+
+# Sudo code:
+# 1 - 
+
+# Work: 
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 16 - May - 2020
+# Source: https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
+# Title: Return the Sum of Two Numbers
+
+# Instructions:
+# Create a function that takes two numbers as arguments and return their sum.
+
+# Examples:
+# addition(3, 2) ➞ 5
+# addition(-3, -6) ➞ -9
+# addition(7, 3) ➞ 10
+
+# Notes:
+# Don't forget to return the result.
+# If you get stuck on a challenge, find help in the Resources tab.
+# If you're really stuck, unlock solutions in the Solutions tab.
+
+# Work: 
+
+# def addition(a, b):
+#     return a + b
+
+# ------------------------------------------------------------------------------------------------------------------
