fea48a8f6d25d2b8280c160ff8c5c7032632aa7e
seanaleid <seanaleid@gmail.com>
2020-05-21 16:41:48 -0400

    Triangular Number Sequence

--- practice01.js
+++ practice01.js
@@ -20,6 +20,29 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 21 - May - 2020
+// Source: https://edabit.com/challenge/RMZiERz2cbjmbXruY
+// Title: Triangular Number Sequence
+
+// Instructions:
+// This Triangular Number Sequence is generated from a pattern of dots that form a triangle. The first 5 numbers of the sequence, or dots, are: 1, 3, 6, 10, 15. Write a function that converts n number of places with its corresponding number.
+
+// Examples:
+// triangle(1) ➞ 1
+// triangle(6) ➞ 21
+// triangle(215) ➞ 23220
+
+// Work: 
+// function triangle(n){
+//     return n*(n+1)/2;
+// }
+
+// console.log(triangle(1));
+// console.log(triangle(6));
+// console.log(triangle(215));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 20 - May - 2020
 // Source: https://edabit.com/challenge/GLbuMfTtDWwDv2F73
 // Title: How Much is True?
