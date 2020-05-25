1995e4caeb094164ed7c11d90928fc4786ebb451
seanaleid <seanaleid@gmail.com>
2020-05-24 22:12:45 -0400

    Rock, Paper, Scissors if else

--- practice01.js
+++ practice01.js
@@ -20,6 +20,52 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 24 - May - 2020
+// Source: uXf7RQ5am84tup7mQ
+// Title: Rock, Paper, Scissors
+
+// Instructions:
+// Create a function which takes two strings (p1 and p2 ⁠— which represent player 1 and 2) as arguments and returns a string stating the winner in a game of Rock, Paper, Scissors.
+// Each argument will contain a single string: "Rock", "Paper", or "Scissors". Return the winner according to the following rules:
+// Rock beats Scissors
+// Scissors beats Paper
+// Paper beats Rock
+// If p1 wins, return the string "The winner is p1". If p2 wins, return the string "The winner is p2" and if p1 and p2 are the same, return "It's a draw".
+
+// Examples:
+// rps("Rock", "Paper") ➞ "The winner is p2"
+// rps("Scissors", "Paper") ➞ "The winner is p1"
+// rps("Paper", "Paper") ➞ "It's a draw"
+
+// Notes:
+// All inputs will be valid strings.
+
+// Work: 
+// function rps(p1, p2){
+    // option 1
+    // if(p1 === p2){
+    //     return "It's a draw";
+    // } else if (p1 === "Rock" && p2 === "Paper"){
+    //     return "The winner is p2";
+    // } else if (p1 === "Rock" && p2 === "Scissors"){
+    //     return "The winner is p1";
+    // } else if (p1 === "Paper" && p2 === "Scissors"){
+    //     return "The winner is p2";
+    // } else if (p1 === "Paper" && p2 === "Rock"){
+    //     return "The winner is p1";
+    // } else if (p1 === "Scissors" && p2 === "Paper"){
+    //     return "The winner is p1";
+    // } else if(p1 === "Scissors" && p2 === "Rock"){
+    //     return "The winner is p2";
+    // };
+// }
+
+// console.log(rps("Rock", "Paper"));
+// console.log(rps("Scissors", "Paper"));
+// console.log(rps("Paper", "Paper"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 23 - May - 2020
 // Source: https://edabit.com/challenge/DdAkLaw4eyuWgCMmF
 // Title: Default Mood
