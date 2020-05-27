de690bdcdae2241d59bb2ed25fa892f6db2203d4
seanaleid <seanaleid@gmail.com>
2020-05-27 00:13:36 -0400

    Find the Largest Number in an Array

--- practice01.js
+++ practice01.js
@@ -20,6 +20,34 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 27 - May - 2020
+// Source: https://edabit.com/challenge/HTbRYCmPwfZGnBcxW
+// Title: Find the Largest Number in an Array
+
+// Instructions:
+// Create a function that takes an array of numbers. Return the largest number in the array.
+
+// Examples:
+// findLargestNum([4, 5, 1, 3]) ➞ 5
+// findLargestNum([300, 200, 600, 150]) ➞ 600
+// findLargestNum([1000, 1001, 857, 1]) ➞ 1001
+
+// Notes:
+// Expect either a positive number or zero (there are no negative numbers).
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// Work: 
+// function findLargestNum(arr){
+//     return arr.sort((a,b) => a - b).pop()
+// }
+
+// console.log(findLargestNum([4, 5, 1, 3]));
+// console.log(findLargestNum([300, 200, 600, 150]));
+// console.log(findLargestNum([1000, 1001, 857, 1]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 26 - May - 2020
 // Source: https://edabit.com/challenge/Wg3pBiH8HQggcfaTg
 // Title: Maximum Possible Total
