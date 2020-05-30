199beffc67d2e1b829a9407cb68939ffdd5c3584
seanaleid <seanaleid@gmail.com>
2020-05-30 14:54:38 -0400

    Task 3

--- practice01.js
+++ practice01.js
@@ -247,6 +247,20 @@ const artists = [
   
 //   console.log(newName(8));
 
+/* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
+ *     (1) artists array
+ *     (2) a number which is the desired index in the array.
+ * getArtistByIndex returns a string in the format `The artist at index {id} is {name}.`
+ * 
+ * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
+ * it will return `The artist at index 0 is Amedeo Modigliani`.
+*/
+function getArtistByIndex(arr, id) {
+    return `The artist at index ${id} is ${arr[id].name}`
+}
+  
+console.log(getArtistByIndex(artists, 3));
+console.log(getArtistByIndex(artists, 4));
 
 // ------------------------------------------------------------------------------------------------------------------
 
