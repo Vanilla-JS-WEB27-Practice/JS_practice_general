1d1e3a7e47c4df93df3b09f2d6c8e345cc00dd7e
seanaleid <seanaleid@gmail.com>
2020-06-08 00:17:39 -0400

    Find the Smallest Number in an Array

--- practice01.js
+++ practice01.js
@@ -20,6 +20,40 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 08 - June - 2020
+// Source: https://edabit.com/challenge/g66EJsbdnSDayyEcK
+// Title: Find the Smallest Number in an Array
+
+// Instructions:
+// Create a function that takes an array of numbers and returns the smallest number in the set.
+
+// Examples:
+// findSmallestNum([34, 15, 88, 2]) ➞ 2
+// findSmallestNum([34, -345, -1, 100]) ➞ -345
+// findSmallestNum([-76, 1.345, 1, 0]) ➞ -76
+// findSmallestNum([0.4356, 0.8795, 0.5435, -0.9999]) ➞ -0.9999
+// findSmallestNum([7, 7, 7]) ➞ 
+
+// Notes:
+// Test cases contain decimals.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// Work: 
+// function findSmallestNum(arr){
+//     return Math.min(...arr);
+// }
+
+// console.log(findSmallestNum([34, 15, 88, 2]));
+// console.log(findSmallestNum([34, -345, -1, 100]));
+// console.log(findSmallestNum([-76, 1.345, 1, 0]));
+// console.log(findSmallestNum([0.4356, 0.8795, 0.5435, -0.9999]));
+// console.log(findSmallestNum([7, 7, 7]));
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+
 // Date: 07 - June - 2020
 // Source: https://edabit.com/challenge/NKknKNfeaJxLDfJuZ
 // Title: Convert Number to Corresponding Month Name
