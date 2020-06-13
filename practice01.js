9a2d8de84c0f917c707bfde6da568b28c191e07a
seanaleid <seanaleid@gmail.com>
2020-06-13 12:40:21 -0400

    Get Sum of People's Budget

--- practice01.js
+++ practice01.js
@@ -20,6 +20,53 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 13 - June - 2020
+// Source: https://edabit.com/challenge/tmnCStcrkdWbreKP5
+// Title: Get Sum of People's Budget
+
+// Instructions:
+// Create the function that takes an array with objects and returns the sum of people's budgets.
+
+// Examples:
+// getBudgets([
+//   { name: "John", age: 21, budget: 23000 },
+//   { name: "Steve",  age: 32, budget: 40000 },
+//   { name: "Martin",  age: 16, budget: 2700 }
+// ]) ➞ 65700
+
+// getBudgets([
+//   { name: "John",  age: 21, budget: 29000 },
+//   { name: "Steve",  age: 32, budget: 32000 },
+//   { name: "Martin",  age: 16, budget: 1600 }
+// ]) ➞ 62600
+
+// Work: 
+// function getBudget(arr){
+//     const budget = []
+    
+//     for(let i = 0; i < arr.length; i++){
+//         budget.push(arr[i].budget);
+//     }
+
+//     const budgetSum = budget.reduce((a, b) => a+b, 0);
+
+//     return budgetSum
+// }
+
+// console.log(getBudget([
+//     { name: "John", age: 21, budget: 23000 },
+//     { name: "Steve",  age: 32, budget: 40000 },
+//     { name: "Martin",  age: 16, budget: 2700 }
+//     ]))
+
+// console.log(getBudget([
+//       { name: "John",  age: 21, budget: 29000 },
+//       { name: "Steve",  age: 32, budget: 32000 },
+//       { name: "Martin",  age: 16, budget: 1600 }
+//     ]))
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 12 - June - 2020
 // Source: https://edabit.com/challenge/H9ohpGMyqJ9bAkqE5
 // Title: Check if String Ending Matches Second String
