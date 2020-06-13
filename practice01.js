e9f717a59ef0214c14f49ef21d6495aecd899cc4
seanaleid <seanaleid@gmail.com>
2020-06-13 12:41:12 -0400

    Eliminate Odd Numbers Within An Array

--- practice01.js
+++ practice01.js
@@ -21,6 +21,35 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 13 - June - 2020
+// Source: https://edabit.com/challenge/uJ9K3HtbjHoDGXKhg
+// Title: Eliminate Odd Numbers within an Array
+
+// Instructions:
+// Create a function that takes an array of numbers and returns only the even values.
+
+// Examples:
+// noOdds([1, 2, 3, 4, 5, 6, 7, 8]) ➞ [2, 4, 6, 8]
+// noOdds([43, 65, 23, 89, 53, 9, 6]) ➞ [6]
+// noOdds([718, 991, 449, 644, 380, 440]) ➞ [718, 644, 380, 440]
+
+// Notes:
+// Return all even numbers in the order they were given.
+// All test cases contain valid numbers ranging from 1 to 3000.
+
+// Work: 
+// function noOdds(arr){
+//     const newArr = arr.filter(item => item % 2 === 0)
+
+//     return newArr;
+// }
+
+// console.log(noOdds([1, 2, 3, 4, 5, 6, 7, 8]));
+// console.log(noOdds([43, 65, 23, 89, 53, 9, 6]));
+// console.log(noOdds([718, 991, 449, 644, 380, 440]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 13 - June - 2020
 // Source: https://edabit.com/challenge/tmnCStcrkdWbreKP5
 // Title: Get Sum of People's Budget
 
