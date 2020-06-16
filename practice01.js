d04a3fbf26f2ebc4d7305e3397d36f3a6ef73e56
seanaleid <seanaleid@gmail.com>
2020-06-16 16:13:34 -0400

    Create a Phone Number

--- practice01.js
+++ practice01.js
@@ -21,6 +21,32 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 16 - June - 2020
+// Source: https://edabit.com/challenge/xDNkweBBNXBMyQRdG
+// Title: Create a Phone Number
+
+// Instructions:
+// Write a function that takes an array of 10 integers (between 0 and 9) and returns a string in form of a phone number.
+
+// Examples:
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
+// createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) ➞ "(111) 111-1111"
+// createPhoneNumber([8, 7, 4, 1, 2, 5, 6, 5, 8, 2]) ➞ "(874) 125-6582"
+
+// Work: 
+// function createPhoneNumber(numbers){
+//         const three = numbers.slice(0,3).join('');
+//         const threeTwo = numbers.slice(3,6).join('');
+//         const four = numbers.slice(6,10).join('');
+//         return `(${three}) ${threeTwo}-${four}`;
+
+    // return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+// }
+
+// console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 16 - June - 2020
 // Source: https://edabit.com/challenge/v9DwaeR6NQoapEvHh
 // Title: Difference of Max and Min Numbers in Array
 
