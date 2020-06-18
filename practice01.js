bc82745586714b588e992f4a8bb0b92b65dc2832
seanaleid <seanaleid@gmail.com>
2020-06-18 18:40:20 -0400

    Fix the Error: Vowel Edition

--- practice01.js
+++ practice01.js
@@ -21,6 +21,35 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 18 - June - 2020
+// Source: https://edabit.com/challenge/KgNMGbXXst7ZdWchR
+// Title: Fix teh Error: Vowel Edition
+
+// Instructions:
+// Your friend is trying to write a function that removes all vowels from a string. They write:
+// function removeVowels(str) {
+//     return str.replace(/[aeiou]/, "")
+//   }
+//   However, it seems that it doesn't work? Fix your friend's code so that it actually does remove all vowels.
+
+// Examples:
+// removeVowels("candy") ➞ "cndy"
+// removeVowels("hello") ➞ "hllo"
+// The "e" is removed, but the "o" is still there!
+// removeVowels("apple") ➞ "pple"
+
+// Work: 
+// Fix this incorrect code, so that all tests pass!
+// function removeVowels(str) {
+//     return str.replace(/[aeiou]/gi, '')
+// };
+
+// console.log(removeVowels('candy'));
+// console.log(removeVowels('hello'));
+// console.log(removeVowels('apple'));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 18 - June - 2020
 // Source: https://edabit.com/challenge/Qbi3GuXjwW5fsoKRn
 // Title: Additive Inverse
 
