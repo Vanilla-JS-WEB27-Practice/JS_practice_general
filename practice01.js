d71c88f28c865d9edf311107e4318ab5c808e6e3
seanaleid <seanaleid@gmail.com>
2020-06-28 21:29:20 -0400

    Return the Time Saved by Speeding

--- practice01.js
+++ practice01.js
@@ -20,6 +20,42 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: - June - 2020
+// Source: https://edabit.com/challenge/fkzJMioMcnP4p4BFG
+// Title: Return the Time Saved by Speeding
+
+// Instructions:
+// One cause for speeding is the desire to shorten the time spent traveling. While in long distance trips speeding does save an appreciable amount of time, the same cannot be said about short distance trips.
+
+// Create a function that calculates the amount of time saved (in minutes) were you traveling with an average speed that is above the speed-limit as compared to traveling with an average speed exactly at the speed-limit.
+
+// Examples:
+// The paramater's format is as follows:
+// (speed limit, avg speed, distance traveled at avg speed)
+
+// timeSaved(80, 90, 40) ➞ 3.3
+
+// timeSaved(80, 90, 4000) ➞ 333.3 
+
+// timeSaved(80, 100, 40 ) ➞ 6.0
+
+// timeSaved(80, 100, 10) ➞ 1.5
+
+// Work: 
+// function timeSaved(limit, avg, distance){
+//     const speedLimit = distance/limit*60;
+//     const fullLimit = speedLimit.split
+//     const avgSpeed = distance/avg*60;
+//     return speedLimit - avgSpeed;
+// }
+
+// console.log(timeSaved(80, 90, 40));
+// console.log(timeSaved(80, 90, 4000));
+// console.log(timeSaved(80, 100, 40));
+// console.log(timeSaved(80, 100, 10));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 19 - June - 2020
 // Source: https://edabit.com/challenge/KffCqRz23XL5ewbgH
 // Title: Reverse Words Starting With a Particular Letter
