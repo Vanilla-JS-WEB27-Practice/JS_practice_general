64dcf1cd66f40c0a8684d0ad071d382430f8d624
seanaleid <seanaleid@gmail.com>
2020-07-03 14:14:40 -0400

    Remove Trailing and Leading Zeros

--- practice01.js
+++ practice01.js
@@ -1,6 +1,6 @@
 
 // Template
-// Date: - June - 2020
+// Date: - July - 2020
 // Source: 
 // Title: 
 
@@ -20,7 +20,40 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
-// Date: - June - 2020
+// Date: 03 - July - 2020
+// Source: https://edabit.com/challenge/3jzycf6fcgwZbvpcf
+// Title: Remove Trailing and Leading Zeros
+
+// Instructions:
+// Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
+// Trailing Zeros are the zeros after a decimal point which don't affect the value (e.g. the last three zeros in 3.4000 and 3.04000).
+// Leading Zeros are the zeros before a whole number which don't affect the value (e.g. the first three zeros in 000234 and 000230).
+
+// Examples:
+// removeLeadingTrailing("230.000") ➞ "230"
+// removeLeadingTrailing("00402") ➞ "402"
+// removeLeadingTrailing("03.1400") ➞ "3.14"
+// removeLeadingTrailing("30") ➞ "30"
+
+// Notes:
+// Return a string.
+// If you get a number with .0 on the end, return the integer value (e.g. return "4" rather than "4.0").
+// If the number is 0, 0.0, 000, 00.00, etc... return "0".
+
+// Work: 
+function removeLeadingTrailing(n){
+    return Math.abs(Number(n)).toString();
+}
+
+console.log(removeLeadingTrailing('230.000'));
+console.log(removeLeadingTrailing('00402'));
+console.log(removeLeadingTrailing("03.1400"));
+
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 02 - July - 2020
 // Source: https://edabit.com/challenge/YJuhHKSmNCaKNHcD3
 // Title: Highest Digit
 // Resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max
@@ -34,12 +67,12 @@
 // highestDigit(377401) ➞ 7
 
 // Work: 
-function highestDigit(str){
-    const arr = str.toString().split('');
-    return Math.max(...arr);
-}
+// function highestDigit(str){
+//     const arr = str.toString().split('');
+//     return Math.max(...arr);
+// }
 
-console.log(highestDigit(379));
+// console.log(highestDigit(379));
 
 // ------------------------------------------------------------------------------------------------------------------
 
