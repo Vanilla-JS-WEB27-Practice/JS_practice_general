cf0b3682b2b6a00ae948819ccfb6ac005c6f8c04
seanaleid <seanaleid@gmail.com>
2020-08-17 16:17:02 -0400

    Print reversed array

--- practice01.py
+++ practice01.py
@@ -20,6 +20,33 @@
 # Work: 
 
 # ------------------------------------------------------------------------------------------------------------------
+# Date: 17 - August - 2020
+# Source: Connor TL practice 
+# Given the following array of values, print out all the elements in reverse order, with each element on a new line.
+# For example, given the list
+# [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+# Your output should be
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# You may use whatever programming language you'd like.
+# Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+# want to print the array in reverse order every num on a new line
+
+# arr = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+# reversed_arr = reversed(arr)
+# for num in reversed_arr:
+#     print(f"\n {num}")
+
 
 # Date: 16 - August - 2020
 # Source: https://edabit.com/challenge/8pDH2SRutPoaQghgc
