75e6bea69cf5986a2fcb03416f524975c7f2d4e6
seanaleid <seanaleid@gmail.com>
2020-08-25 12:06:48 -0400

    How many numbers are smaller than the current number

--- practice01.py
+++ practice01.py
@@ -21,6 +21,59 @@
 
 # ------------------------------------------------------------------------------------------------------------------
 
+# Date: 25 - August - 2020
+# Source: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+# Title: How Many Numbers Are Smaller Than the Current Number
+# Resources: 
+
+# Instructions:
+# Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. 
+# That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+# Return the answer in an array.
+
+# Examples:
+
+# Example 1:
+# Input: nums = [8,1,2,2,3]
+# Output: [4,0,1,1,3]
+# Explanation: 
+# For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+# For nums[1]=1 does not exist any smaller number than it.
+# For nums[2]=2 there exist one smaller number than it (1). 
+# For nums[3]=2 there exist one smaller number than it (1). 
+# For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+# Example 2:
+# Input: nums = [6,5,4,8]
+# Output: [2,1,0,3]
+
+# Example 3:
+# Input: nums = [7,7,7,7]
+# Output: [0,0,0,0]
+
+# Notes:
+# 2 <= nums.length <= 500
+# 0 <= nums[i] <= 100
+
+# Work: 
+# def smallerNumbersThanCurrent(nums):
+#     counter_d = {num:nums.count(num) for num in set(nums)}
+
+#     result = list()
+#     for num in nums:
+#         count = 0
+#         for counter in counter_d:
+#             if num > counter:
+#                 count+=counter_d[counter]
+#         result.append(count)
+#     return counter_d
+
+
+# nums = [6,5,4,8]
+# print(smallerNumbersThanCurrent(nums))
+
+# ------------------------------------------------------------------------------------------------------------------
+
 # Date: 24 - August - 2020
 # Source: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
 # Title: Number of Steps to Reduce a Number to Zero
