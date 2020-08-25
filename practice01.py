57e16131b7a2b627ce8cec4aadb20720f1a0bf13
seanaleid <seanaleid@gmail.com>
2020-08-25 13:34:12 -0400

    Shuffle String

--- practice01.py
+++ practice01.py
@@ -22,6 +22,70 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 25 - August - 2020
+# Source: https://leetcode.com/problems/shuffle-string/
+# Title: Shuffle String
+# Resources: 
+
+# Instructions:
+# Given a string s and an integer array indices of the same length.
+# The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+# Return the shuffled string.
+
+# Examples:
+
+# Example 1:
+# Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+# Output: "leetcode"
+# Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+
+# Example 2:
+# Input: s = "abc", indices = [0,1,2]
+# Output: "abc"
+# Explanation: After shuffling, each character remains in its position.
+
+# Example 3:
+# Input: s = "aiohn", indices = [3,1,4,2,0]
+# Output: "nihao"
+
+# Example 4:
+# Input: s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
+# Output: "arigatou"
+
+# Example 5:
+# Input: s = "art", indices = [1,0,2]
+# Output: "rat"
+
+# Notes:
+# s.length == indices.length == n
+# 1 <= n <= 100
+# s contains only lower-case English letters.
+# 0 <= indices[i] < n
+# All values of indices are unique (i.e. indices is a permutation of the integers from 0 to n - 1).
+
+# Work: 
+# def restoreString(s, indices):
+#     newS = [char for char in s]
+#     d = dict()
+#     idx = 0
+#     for num in indices:
+#         d[num] = newS[idx]
+#         idx+=1
+#     sorted_word = sorted(d.items(), key=lambda x: x[0])
+
+#     result = ' '
+#     for i in sorted_word:
+#         result+=i[1]
+    
+#     return result
+
+
+# s = 'codeleet'
+# indices = [4,5,6,7,0,2,1,3]
+# print(restoreString(s, indices))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 25 - August - 2020
 # Source: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
 # Title: How Many Numbers Are Smaller Than the Current Number
 # Resources: 
