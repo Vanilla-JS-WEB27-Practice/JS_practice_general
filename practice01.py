99386f887fabdead62b0f8ff1d15bbf08f40195f
seanaleid <seanaleid@gmail.com>
2020-08-24 21:59:04 -0400

    Number of Good Pairs

--- practice01.py
+++ practice01.py
@@ -22,6 +22,51 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 24 - August - 2020
+# Source: https://leetcode.com/problems/number-of-good-pairs/
+# Title: Number of Good Pairs
+# Resources: 
+
+# Instructions:
+# Given an array of integers nums.
+# A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+# Return the number of good pairs.
+
+# Examples:
+
+# Example 1:
+# Input: nums = [1,2,3,1,1,3]
+# Output: 4
+# Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+# Example 2:
+# Input: nums = [1,1,1,1]
+# Output: 6
+# Explanation: Each pair in the array are good.
+
+# Example 3:
+# Input: nums = [1,2,3]
+# Output: 0
+
+# Notes:
+# 1 <= nums.length <= 100
+# 1 <= nums[i] <= 100
+
+# Work: 
+def numIdenticalPairs(nums):
+    count = 0
+    for i, val in enumerate(nums):
+        for j, val in enumerate(nums):
+            if nums[i] == nums[j] and i < j:
+                count+=1
+    return count
+
+nums = [1,2,3]
+print(numIdenticalPairs(nums))
+
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 24 - August - 2020
 # Source: https://leetcode.com/problems/shuffle-the-array/
 # Title: Shuffle the Array
 # Resources: 
