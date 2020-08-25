4d3d87a5bd91c5d38478920e9305a7f1b5cc7bca
seanaleid <seanaleid@gmail.com>
2020-08-25 13:49:47 -0400

    Subtract the Product and Sum of Digits of an Integer

--- practice01.py
+++ practice01.py
@@ -22,6 +22,68 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 25 - August - 2020
+# Source: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+# Title: Subtract the Product and Sum of Digits of an Integer
+# Resources: 
+
+# Instructions:
+# Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+# Examples:
+
+# Example 1:
+# Input: n = 234
+# Output: 15 
+# Explanation: 
+# Product of digits = 2 * 3 * 4 = 24 
+# Sum of digits = 2 + 3 + 4 = 9 
+# Result = 24 - 9 = 15
+
+# Example 2:
+# Input: n = 4421
+# Output: 21
+# Explanation: 
+# Product of digits = 4 * 4 * 2 * 1 = 32 
+# Sum of digits = 4 + 4 + 2 + 1 = 11 
+# Result = 32 - 11 = 21
+
+# Notes:
+# 1 <= n <= 10^5
+
+# Work: 
+# import math
+# def subtractProductAndSum(n):
+    # FINAL PASS --> better
+    # newN = [int(char) for char in str(n)]
+
+    # return math.prod(newN) - sum(newN)
+    
+    # SECOND PASS --> not very fast
+    # newN = [int(char) for char in str(n)]
+    #     productN = 1
+    #     for num in newN:
+    #         productN*=int(num)
+
+    #     return productN - sum(newN)
+    
+    # FIRST PASS
+    # newN = [char for char in str(n)]
+    # productN = 1
+    # for num in newN:
+    #     productN *= int(num)
+
+    # sumN = 0
+    # for num in newN:
+    #     sumN+=int(num)
+
+    # return productN - sumN
+
+# n = 222
+# print(subtractProductAndSum(n))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 25 - August - 2020
 # Source: https://leetcode.com/problems/shuffle-string/
 # Title: Shuffle String
 # Resources: 
