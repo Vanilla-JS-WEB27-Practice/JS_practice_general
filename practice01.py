573f713f875cc7f42d6e15457071bbf5c6a5e05b
seanaleid <seanaleid@gmail.com>
2020-08-24 21:11:47 -0400

    Shuffle the Array

--- practice01.py
+++ practice01.py
@@ -21,10 +21,58 @@
 
 # ------------------------------------------------------------------------------------------------------------------
 
-# Date: - August - 2020
+# Date: 24 - August - 2020
+# Source: https://leetcode.com/problems/shuffle-the-array/
+# Title: Shuffle the Array
+# Resources: 
+
+# Instructions:
+# Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+# Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+# Examples:
+
+# Example 1:
+# Input: nums = [2,5,1,3,4,7], n = 3
+# Output: [2,3,5,4,1,7] 
+# Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+# Example 2:
+# Input: nums = [1,2,3,4,4,3,2,1], n = 4
+# Output: [1,4,2,3,3,2,4,1]
+
+# Example 3:
+# Input: nums = [1,1,2,2], n = 2
+# Output: [1,2,1,2]
+
+# Notes:
+# 1 <= n <= 500
+# nums.length == 2n
+# 1 <= nums[i] <= 10^3
+
+# Work: 
+# def shuffle(nums, n):
+#     shuffled = list()
+#     for i, val in enumerate(nums):
+#         if i >= n:
+#             break
+#         else:
+#             shuffled.append(nums[i])
+#             shuffled.append(nums[i+n])
+#     return shuffled
+
+
+# nums = [2,5,1,3,4,7]
+# n = 3
+
+# print(shuffle(nums, n))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 23 - August - 2020
 # Source: https://edabit.com/challenge/pfn6QRn6eiTHEPpSs
 # Title: Drunken Python
-# Resources: 
+# Resources: https://realpython.com/convert-python-string-to-int/
 
 # Instructions:
 # Python got drunk and the built-in functions str() and int() are acting odd:
@@ -49,19 +97,19 @@
 # 1 - 
 
 # Work: 
-str, int = int, str
+# str, int = int, str
 
-def int_to_str(num):
-    return str(num)
+# def int_to_str(num):
+#     return str(num)
     # return "'{}'".format(num)
 
-def str_to_int(num):
-    return int(num)
+# def str_to_int(num):
+#     return int(num)
     # return ("%d" % int(num))
     # return ("{:d}".format(int(num)))
 
-print(int_to_str(100))
-print(str_to_int('7'))
+# print(int_to_str(100))
+# print(str_to_int('7'))
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 22 - August - 2020
