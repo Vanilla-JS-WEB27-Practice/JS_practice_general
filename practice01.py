ce4f7b7f61aa3b6b6c084aabdc6c506d9135429d
seanaleid <seanaleid@gmail.com>
2020-08-25 22:43:55 -0400

    Decompress Run-Length Encoded List

--- practice01.py
+++ practice01.py
@@ -21,6 +21,59 @@
 
 # ------------------------------------------------------------------------------------------------------------------
 
+# Date: - August - 2020
+# Source: https://leetcode.com/problems/decompress-run-length-encoded-list/
+# Title: Decompress Run-Length Encoded List
+# Resources: 
+
+# Instructions:
+# We are given a list nums of integers representing a list compressed with run-length encoding.
+# Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  
+# For each such pair, there are freq elements with value val concatenated in a sublist. 
+# Concatenate all the sublists from left to right to generate the decompressed list.
+# Return the decompressed list.
+
+# Examples:
+
+# Example 1:
+# Input: nums = [1,2,3,4]
+# Output: [2,4,4,4]
+# Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
+# The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
+# At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+
+# Example 2:
+# Input: nums = [1,1,2,3]
+# Output: [1,3,3]
+
+# Notes:
+# 2 <= nums.length <= 100
+# nums.length % 2 == 0
+# 1 <= nums[i] <= 100
+
+# Work: 
+# def decompressRLEList(nums):
+#     l = list()
+#     start = 0
+#     end = 2
+#     lenN = len(nums) / 2
+#     result = list()
+#     while len(l) < lenN:
+#         l.append(nums[start:end])
+#         start+=2
+#         end+=2
+
+#     for n in l:
+#         result_val = [n[1]]*n[0]
+#         result.extend(result_val)
+
+#     return result
+
+# nums = [1,1,2,3]
+# print(decompressRLEList(nums))
+
+# ------------------------------------------------------------------------------------------------------------------
+
 # Date: 25 - August - 2020
 # Source: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
 # Title: Subtract the Product and Sum of Digits of an Integer
