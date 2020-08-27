fe0cfc397dd2f1a6e5e5e6786c60d2c68042559f
seanaleid <seanaleid@gmail.com>
2020-08-27 12:15:05 -0400

    Split a String in Balanced Strings

--- practice01.py
+++ practice01.py
@@ -22,6 +22,73 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 26 - August - 2020
+# Source: https://leetcode.com/problems/split-a-string-in-balanced-strings/
+# Title: Split a String in Balanced Strings
+# Resources: 
+
+# Instructions:
+# Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+# Given a balanced string s split it in the maximum amount of balanced strings.
+# Return the maximum amount of splitted balanced strings.
+
+# Examples:
+# Example 1:
+# Input: s = "RLRRLLRLRL"
+# Output: 4
+# Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+# Example 2:
+# Input: s = "RLLLLRRRLR"
+# Output: 3
+# Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
+# Example 3:
+# Input: s = "LLLLRRRR"
+# Output: 1
+# Explanation: s can be split into "LLLLRRRR".
+
+# Example 4:
+# Input: s = "RLRRRLLRLL"
+# Output: 2
+# Explanation: s can be split into "RL", "RRRLLRLL", since each substring contains an equal number of 'L' and 'R'
+
+# Notes:
+# 1 <= s.length <= 1000
+# s[i] = 'L' or 'R'
+
+# Work: 
+def balancedStringSplit(s):
+    i=l=r=count=0
+    while i < len(s):
+        if s[i] == "L":
+            l+=1
+        else:
+            r+=1
+        if l==r:
+            count+=1
+            r=l=0
+        i+=1
+    return count
+    
+    # newS = {i:char for i, char in enumerate(s)}
+    # result = list()
+    
+    # for k, v in newS.items():
+        # print(f"{newS[k]}, {newS[k+1]}")
+        # count = 0
+        # if newS[k] != newS[k+1]:
+        #     print(f"YES")
+    
+    # for char in newS:
+    #     print(f'{char}{newS[1]}')
+    # return newS
+
+s = "RLLLLRRRLR"
+print(balancedStringSplit(s))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 26 - August - 2020
 # Source: https://leetcode.com/problems/create-target-array-in-the-given-order/
 # Title: Create Target Array in the Given Order
 # Resources: 
