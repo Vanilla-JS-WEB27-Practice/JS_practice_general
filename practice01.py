7d89e6dce6de13491f0d0bdf4f228e09ef5349a5
seanaleid <seanaleid@gmail.com>
2020-08-29 21:44:09 -0400

    Calculate the Profit

--- practice01.py
+++ practice01.py
@@ -22,6 +22,56 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 29 - August - 2020
+# Source: https://edabit.com/challenge/YfoKQWNeYETb9PYpw
+# Title: Calculate the Profit
+# Resources: 
+
+# Instructions:
+# You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. 
+# You are given a dictionary containing the cost price per unit (in dollars), sell price per unit (in dollars), and the starting inventory. 
+# Return the total profit made, rounded to the nearest dollar.
+
+# Examples:
+# profit({
+#   "cost_price": 32.67,
+#   "sell_price": 45.00,
+#   "inventory": 1200
+# }) ➞ 14796
+
+# profit({
+#   "cost_price": 225.89,
+#   "sell_price": 550.00,
+#   "inventory": 100
+# }) ➞ 32411
+
+# profit({
+#   "cost_price": 2.77,
+#   "sell_price": 7.95,
+#   "inventory": 8500
+# }) ➞ 44030
+
+# Notes:
+# Assume all inventory has been sold.
+# Profit = Total Sales - Total Cost
+
+# Work: 
+# def profit(info):
+#     cost = info["cost_price"]*info["inventory"]
+#     sell = info["sell_price"]*info["inventory"]s
+#     profit_amount = sell - cost
+#     return round(profit_amount)
+
+
+# info = {
+#     "cost_price": 32.67,
+#     "sell_price": 45.00,
+#     "inventory": 1200
+# }
+# print(profit(info))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 29 - August - 2020
 # Source: https://edabit.com/challenge/BuwHwPvt92yw574zB
 # Title: List of Multiples
 # Resources: 
