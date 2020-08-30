03913dc0c96d4986655bd76db72a205df403220f
seanaleid <seanaleid@gmail.com>
2020-08-29 23:20:21 -0400

    A Circle and Two Squares

--- practice01.py
+++ practice01.py
@@ -21,6 +21,33 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 29 - August - 2020
+# Source: https://edabit.com/challenge/NNhkGocuPMcryW7GP
+# Title: A Circle and Two Squares
+# Resources: https://www.geeksforgeeks.org/area-square-circumscribed-circle/
+
+# Instructions:
+# Imagine a circle and two squares: a smaller and a bigger one. For the smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+# Create a function, that takes an integer (radius of the circle) and returns the difference of the areas of the two squares.
+
+# Examples:
+# square_areas_difference(5) ➞ 50
+# square_areas_difference(6) ➞ 72
+# square_areas_difference(7) ➞ 98
+
+# Notes:
+# Use only positive integer parameters.
+
+# Work: 
+# def square_areas_difference(r):
+#     return 2*r*r
+
+# print(square_areas_difference(5))
+# print(square_areas_difference(6))    
+# print(square_areas_difference(7))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 29 - August - 2020
 # Source: https://edabit.com/challenge/co4nwXSvnCjGEu8vp
 # Title: Date Format
 
