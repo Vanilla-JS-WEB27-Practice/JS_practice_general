57746074fe4f63c53842bdc6c7fd050d86c73740
seanaleid <seanaleid@gmail.com>
2020-08-29 22:11:30 -0400

    You FAILEDPASSED the Exam

--- practice01.py
+++ practice01.py
@@ -1,4 +1,3 @@
-
 # Template
 # Date: - August - 2020
 # Source: 
@@ -18,6 +17,40 @@
 # 1 - 
 
 # Work: 
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 29 - August - 2020
+# Source: https://edabit.com/challenge/YEeywMm2eMtae4WDW
+# Title: You FAILEDPASSED the Exam
+
+# Instructions:
+# The challenge is to fix all of the bugs in this incredibly messy code, which the code in the image might've actually looked like (probably not)! The code given will output the same middle two lines as in the image shown above.
+# First parameter is the user's score.
+# Second parameter is the required score.
+
+# Examples:
+# grade_percentage("85%", "85%") ➞ "You PASSED the Exam"
+# grade_percentage("99%", "85%") ➞ "You PASSED the Exam"
+# grade_percentage("65%", "90%") ➞ "You FAILED the Exam"
+
+# Notes:
+# Note that inputs will be given as a string percentage number.
+# Maintain all capitalization.
+# Feel free to declutter and refactor code if it helps!
+
+# Work: 
+# def grade_percentage(user_score, pass_score):
+#     if int(user_score[:-1]) < int(pass_score[:-1]):
+#         return 'You FAILED the Exam'
+#     if int(user_score[:-1]) == int(pass_score[:-1]):
+#         return 'You PASSED the Exam'
+#     if int(user_score[:-1]) >= int(pass_score[:-1]):
+#         return 'You PASSED the Exam'
+
+# print(grade_percentage("85%", "85%"))
+# print(grade_percentage("99%", "85%"))
+# print(grade_percentage("65%", "90%"))
 
 # ------------------------------------------------------------------------------------------------------------------
 
