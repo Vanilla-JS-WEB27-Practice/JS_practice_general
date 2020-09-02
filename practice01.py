351fb18bb0e310625514cd66f607693c67f124fa
seanaleid <seanaleid@gmail.com>
2020-09-02 17:04:50 -0400

    Luke, I Am Your...

--- practice01.py
+++ practice01.py
@@ -21,6 +21,40 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 02 - September - 2020
+# Source: https://edabit.com/challenge/8pDH2SRutPoaQghgc
+# Title: Luke, I Am Your ...
+# Resources: 
+
+# Instructions:
+# Luke Skywalker has family and friends. Help him remind them who is who. Given a string with a name, return the relation of that person to Luke.
+
+# Person	    Relation
+# Darth Vader	father
+# Leia	        sister
+# Han	        brother in law
+# R2D2	        droid
+
+# Examples:
+# relation_to_luke("Darth Vader") ➞ "Luke, I am your father."
+# relation_to_luke("Leia") ➞ "Luke, I am your sister."
+# relation_to_luke("Han") ➞ "Luke, I am your brother in law."
+
+# Work: 
+# def relation_to_luke(name):
+#     d = {
+#         "Darth Vader": "father",
+#         "Leia": "sister",
+#         "Han": "brother in law",
+#         "R2D2": "droid"
+#     }
+#     return "Luke, I am your "+d[name]+"."
+    # return f"Luke, I am your {d[name]}."
+
+# print(relation_to_luke("Leia"))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 02 - September - 2020
 # Source: https://edabit.com/challenge/6dnhESWBcTMMF3gsa
 # Title: Stupid Addition
 
