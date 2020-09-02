f1fcf579dac73b8d8d106a6a777caefdbdd8b7b1
seanaleid <seanaleid@gmail.com>
2020-09-02 16:48:54 -0400

    Stupid Addition

--- practice01.py
+++ practice01.py
@@ -21,6 +21,39 @@
 # ------------------------------------------------------------------------------------------------------------------
 
 # Date: 02 - September - 2020
+# Source: https://edabit.com/challenge/6dnhESWBcTMMF3gsa
+# Title: Stupid Addition
+
+# Instructions:
+# Create a function that takes two parameters and, if both parameters are strings, 
+# add them as if they were integers or if the two parameters are integers, concatenate them.
+
+# Examples:
+# stupid_addition(1, 2) ➞ "12"
+# stupid_addition("1", "2") ➞ 3
+# stupid_addition("1", 2) ➞ None
+
+# Notes:
+# If the two parameters are different data types, return None.
+# All parameters will either be strings or integers.
+
+# Work: 
+# def stupid_addition(a,b):
+#     if type(a) == type(b):
+#         if type(a) == str:
+#             return int(a)+int(b)
+#         else:
+#             return str(a)+str(b)
+#     else:
+#         return None
+
+# print(stupid_addition(1,2))
+# print(stupid_addition("1","2"))
+# print(stupid_addition("1",2))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Date: 02 - September - 2020
 # Source: https://edabit.com/challenge/QgSMSMpfcEebAyCye
 # Title: Return the Time Saved by Speeding
 
