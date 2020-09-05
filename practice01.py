cafb6023d8e3b9d445d4bee58f434c5edfcfba63
seanaleid <seanaleid@gmail.com>
2020-09-04 22:05:40 -0400

    International Greetings

--- practice01.py
+++ practice01.py
@@ -20,6 +20,52 @@
 
 # ------------------------------------------------------------------------------------------------------------------
 
+# Date: 04 - September - 2020
+# Source: https://edabit.com/challenge/vAS4Hp4wzSEnQs3tZ
+# Title: International Greetings
+# Resources: 
+
+# Instructions:
+# Suppose you have a guest list of students and the country they are from, stored as key-value pairs in a dictionary.
+# GUEST_LIST = {
+# "Randy": "Germany",
+# "Karla": "France",
+# "Wendy": "Japan",
+# "Norman": "England",
+# "Sam": "Argentina"
+# }
+# Write a function that takes in a name and returns a name tag, that should read:
+# "Hi! I'm [name], and I'm from [country]."
+# If the name is not in the dictionary, return:
+# "Hi! I'm a guest."
+
+# Examples:
+# greeting("Randy") ➞ "Hi! I'm Randy, and I'm from Germany."
+# greeting("Sam") ➞ "Hi! I'm Sam, and I'm from Argentina."
+# greeting("Monti") ➞ "Hi! I'm a guest."
+
+# Work: 
+GUEST_LIST = {
+	"Randy": "Germany", 
+	"Karla": "France", 
+	"Wendy": "Japan", 
+	"Norman": "England", 
+	"Sam": "Argentina"
+}
+
+def greeting(name):
+    if name not in GUEST_LIST:
+        return "Hi! I'm a guest."
+        # return f"Hi! I'm a guest."
+    else:
+        return "Hi! I'm "+name+", and I'm from"+GUEST_LIST[name]+"."
+        # return f"Hi! I'm {name}, and I'm from {GUEST_LIST[name]}"
+
+print(greeting('Randy'))
+print(greeting('Sam'))
+print(greeting('Monti'))
+# ------------------------------------------------------------------------------------------------------------------
+
 # Date: 02 - September - 2020
 # Source: https://edabit.com/challenge/HjpihSKFBfRCCg86J
 # Title: Compound Interest
