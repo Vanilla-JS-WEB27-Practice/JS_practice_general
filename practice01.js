2e802d775d3ffe58f1d62e518702efb7d3747bba
seanaleid <seanaleid@gmail.com>
2020-09-29 21:35:00 -0400

    Truthy or Falsy?

--- practice01.js
+++ practice01.js
@@ -20,6 +20,47 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 29 - September - 2020
+// Source: https://edabit.com/challenge/czEAQio8zr9HjZsQn
+// Title: Truthy or Falsy?
+
+// Instructions:
+// A value is said to be "truthy" if it evaluates to true in a Boolean context.
+// All values are truthy in JavaScript unless they're one of the following:
+// false
+// null
+// undefined
+// 0
+// NaN
+// ""
+// In JavaScript, an empty object and an empty array are both considered "truthy,"
+// but an empty string is considered false when evaluated as a Boolean (this behavior is what we call "falsey").
+// Create a function that takes an argument of any data type and returns 1 if it's truthy and 0 if it's falsy.
+
+// Examples:
+// isTruthy(0) ➞ 0
+// isTruthy(false) ➞ 0
+// isTruthy("") ➞ 0
+// isTruthy("false") ➞ 1
+
+// Notes:
+// You may have to take into account NaN's unique behavior in JavaScript.
+// While other falsey values are equal to themselves, NaN != NaN
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// Work:
+// function isTruthy(input) {
+//   return !!input ? 1 : 0;
+// }
+
+// console.log(isTruthy(0));
+// console.log(isTruthy(false));
+// console.log(isTruthy(""));
+// console.log(isTruthy("false"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 29 - September - 2020
 // Source: https://edabit.com/challenge/humnHvXGXnBnStg5F
 // Title: Trace That Matrix
 
