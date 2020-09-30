4d8128dd3c00b7b0cf8c8e866b2968f8158dea0f
seanaleid <seanaleid@gmail.com>
2020-09-29 21:47:44 -0400

    Return Last Index

--- practice01.js
+++ practice01.js
@@ -20,6 +20,41 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 29 - September - 2020
+// Source: https://edabit.com/challenge/MT5SbF9Xg5Dku6bqL
+// Title: Return Last Index
+
+// Instructions:
+// Create a function that returns the last value of the last item in an array or string.
+
+// Examples:
+// lastItem([0, 4, 19, 34, 50, -9, 2]) ➞ 2
+// lastItem("The quick brown fox jumped over the lazy dog") ➞ "g"
+// lastItem([]) ➞ undefined
+
+// Notes:
+// Arrays/strings will be of varying size.
+// Return undefined if array/string is emtpy.
+
+// Work:
+// function lastItem(input) {
+//   const emptyStr = "";
+//   const emptyArr = [];
+//   if (input === emptyArr || input === emptyStr) {
+//     return undefined;
+//   } else if (typeof input === typeof emptyStr) {
+//     return input.split("")[input.length - 1];
+//   } else {
+//     return input[input.length - 1];
+//   }
+// }
+
+// console.log(lastItem([0, 4, 19, 34, 50, -9, 2]));
+// console.log(lastItem("The quick brown fox jumped over the lazy dog"));
+// console.log(lastItem([]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 29 - September - 2020
 // Source: https://edabit.com/challenge/czEAQio8zr9HjZsQn
 // Title: Truthy or Falsy?
 
