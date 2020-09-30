51829918836d4dc6a622db81b34d4af40213be36
seanaleid <seanaleid@gmail.com>
2020-09-29 21:22:34 -0400

    Trace That Matrix

--- practice01.js
+++ practice01.js
@@ -19,7 +19,92 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
-// Date: - September - 2020
+// Date: 29 - September - 2020
+// Source: https://edabit.com/challenge/humnHvXGXnBnStg5F
+// Title: Trace That Matrix
+
+// Instructions:
+// Given a square matrix (i.e. same number of rows as columns),
+// its trace is the sum of the entries in the main diagonal
+// (i.e. the diagonal line from the top left to the bottom right).
+// As an example, for:
+// [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+//   ]
+//   ... the trace is 1 + 5 + 9 = 15.
+//   Write a function that takes a square matrix and computes its trace.
+
+// Examples:
+// trace([
+//   [1, 4],
+//   [4, 1]
+// ]) ➞ 2
+
+// // 1 + 1 = 2
+
+// trace([
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]) ➞ 15
+
+// // 1 + 5 + 9 = 15
+
+// trace([
+//   [1, 0, 1, 0],
+//   [0, 2, 0, 2],
+//   [3, 0, 3, 0],
+//   [0, 4, 0, 4]
+// ]) ➞ 10
+
+// 1 + 2 + 3 + 4 = 10
+
+// Notes:
+// As in the examples, the size of the matrices will vary (but they will always be square).
+
+// Work:
+// function trace(arr) {
+//   let arrCount = -1;
+//   let itemCount = -1;
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     arrCount += 1;
+//     itemCount += 1;
+//     result.push(arr[arrCount][itemCount]);
+//   }
+
+//   return result.reduce((a, b) => a + b);
+// }
+
+// console.log(
+//   trace([
+//     [1, 4],
+//     [4, 1],
+//   ])
+// );
+
+// console.log(
+//   trace([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ])
+// );
+
+// console.log(
+//   trace([
+//     [1, 0, 1, 0],
+//     [0, 2, 0, 2],
+//     [3, 0, 3, 0],
+//     [0, 4, 0, 4],
+//   ])
+// );
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 28 - September - 2020
 // Source: https://edabit.com/challenge/PWqkt9HiLcJSr6QEY
 // Title: Sum of Number Elements in an Array
 
