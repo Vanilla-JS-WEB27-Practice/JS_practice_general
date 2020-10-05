b3905b08fe21df4e8e01320c7ac731f91c3e76e0
seanaleid <seanaleid@gmail.com>
2020-10-04 22:40:01 -0400

    Number of Boomerangs

--- practice01.js
+++ practice01.js
@@ -19,6 +19,64 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 04 - October - 2020
+// Source: https://edabit.com/challenge/b7iHQDw72zzkmgCun
+// Title: Number of Boomerangs
+
+// Instructions:
+// A boomerang is a V-shaped sequence that is either upright or upside down.
+// Specifically, a boomerang can be defined as: sub-array of length 3, with the first and last digits being the same and the middle digit being different.
+
+// Some boomerang examples:
+
+// [3, 7, 3], [1, -1, 1], [5, 6, 5]
+// Create a function that returns the total number of boomerangs in an array.
+
+// To illustrate:
+
+// [3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]
+// 3 boomerangs in this sequence:  [3, 7, 3], [1, 5, 1], [2, -2, 2]
+// Be aware that boomerangs can overlap, like so:
+
+// [1, 7, 1, 7, 1, 7, 1]
+// 5 boomerangs (from left to right): [1, 7, 1], [7, 1, 7], [1, 7, 1], [7, 1, 7], and [1, 7, 1]
+
+// Examples:
+// countBoomerangs([9, 5, 9, 5, 1, 1, 1]) ➞ 2
+// countBoomerangs([5, 6, 6, 7, 6, 3, 9]) ➞ 1
+// countBoomerangs([4, 4, 4, 9, 9, 9, 9]) ➞ 0
+
+// Notes:
+// [5, 5, 5] (triple identical digits) is NOT considered a boomerang because the middle digit is identical to the first and last.
+
+// Work:
+// function countBoomerangs(arr) {
+//   const boomerangs = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     boomerangs.push(arr.slice(i, i + 3));
+//   }
+
+//   let result = 0;
+
+//   for (let i = 0; i < boomerangs.length; i++) {
+//     if (boomerangs[i].length === 3) {
+//       if (
+//         boomerangs[i][0] === boomerangs[i][2] &&
+//         boomerangs[i][0] != boomerangs[i][1]
+//       ) {
+//         result += 1;
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(countBoomerangs([9, 5, 9, 5, 1, 1, 1]));
+// console.log(countBoomerangs([5, 6, 6, 7, 6, 3, 9]));
+// console.log(countBoomerangs([4, 4, 4, 9, 9, 9, 9]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 01 - October - 2020
 // Source: https://edabit.com/challenge/uRJKbFSkfa6sXeHZi
 // Title: Convert Yen to USD
