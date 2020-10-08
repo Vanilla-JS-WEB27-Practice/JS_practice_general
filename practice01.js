52d24504db3463a2723aa101a1dac02c9a3bb057
seanaleid <seanaleid@gmail.com>
2020-10-07 22:38:12 -0400

    Repeat the Same Item Multiple Times

--- practice01.js
+++ practice01.js
@@ -20,6 +20,41 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 07 - October - 2020
+// Source: https://edabit.com/challenge/BEHiM4RTmhcLX7Cq6
+// Title: Repeat the Same Item Multiple Times
+
+// Instructions:
+// Create a function that takes two arguments (item, times).
+// The first argument (item) is the item that needs repeating while the second argument (times) is the number of times the item is to be repeated.
+// Return the result in an array.
+
+// Examples:
+// repeat("edabit", 3) ➞ ["edabit", "edabit", "edabit"]
+// repeat(13, 5) ➞ [13, 13, 13, 13, 13]
+// repeat("7", 2) ➞ ["7", "7"]
+// repeat(0, 0) ➞ []
+
+// Notes:
+// item can be either a string or a number.
+// times will always be a number.
+
+// Work:
+// function repeat(item, times) {
+//   const result = [];
+//   while (result.length < times) {
+//     result.push(item);
+//   }
+//   return result;
+// }
+
+// console.log(repeat("edabit", 3));
+// console.log(repeat(13, 5));
+// console.log(repeat("7", 2));
+// console.log(repeat(0, 0));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 07 - October - 2020
 // Source: https://edabit.com/challenge/eitMRpuJR3Ls4zQsn
 // Title: Shuffle the Name
 
