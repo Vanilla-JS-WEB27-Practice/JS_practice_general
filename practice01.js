30bba8a4dd4e5493703a2bf3912e5d67c17b8adc
seanaleid <seanaleid@gmail.com>
2020-10-07 22:21:19 -0400

    Shuffle the Name

--- practice01.js
+++ practice01.js
@@ -20,6 +20,34 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 07 - October - 2020
+// Source: https://edabit.com/challenge/eitMRpuJR3Ls4zQsn
+// Title: Shuffle the Name
+
+// Instructions:
+// Create a function that accepts a string (of a person's first and last name) and returns a string with the first and last name swapped.
+
+// Examples:
+// nameShuffle("Donald Trump") ➞ "Trump Donald"
+// nameShuffle("Rosie O'Donnell") ➞ "O'Donnell Rosie"
+// nameShuffle("Seymour Butts") ➞ "Butts Seymour"
+
+// Notes:
+// There will be exactly one space between the first and last name.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// Work:
+// function nameShuffle(str) {
+//   return str.split(" ").reverse().join(" ");
+// }
+
+// console.log(nameShuffle("Donald Trump"));
+// console.log(nameShuffle("Rosie O'Donnell"));
+// console.log(nameShuffle("Seymour Butts"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 07 - October - 2020
 // Source: https://edabit.com/challenge/RXNybvGHLuvFiWKvq
 // Title: Filter by Digit Length
 
