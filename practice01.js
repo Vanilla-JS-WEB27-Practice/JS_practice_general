7fde9ae6d4a2daab6eb85552446d1e6fe1531643
seanaleid <seanaleid@gmail.com>
2020-10-20 00:00:25 -0400

    Video Streaming Plans

--- practice01.js
+++ practice01.js
@@ -20,6 +20,60 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 19 - October - 2020
+// Source: https://edabit.com/challenge/SrCTGbhwPRyHZSAGu
+// Title: Video Streaming Plans
+
+// Instructions:
+// Given a class for a BasicPlan, write the classes for StandardPlan and PremiumPlan which have class properties of the following:
+
+// BasicPlan	StandardPlan	PremiumPlan
+// ✓	        ✓	            ✓	          canStream
+// ✓	        ✓	            ✓	          canDownload
+// ✓	        ✓	            ✓	          hasSD
+//            ✓	            ✓	          hasHD
+//                          ✓	          hasUHD
+// 1	        2	            4	          numOfDevices
+// $8.99	    $12.99	      $15.99	    price
+
+// Examples:
+// BasicPlan.hasSD ➞ true
+// PremiumPlan.hasSD ➞ true
+// BasicPlan.hasUHD ➞ false
+// BasicPlan.price ➞ '$8.99'
+// PremiumPlan.numOfDevices ➞ 4
+
+// Notes:
+// Try to extend the classes to complete the challenge!
+// If you're unsure what that means, try checking out the JavaScript class tutorials in the Resources tab.
+
+// Work:
+// class BasicPlan {
+//   static canStream = true;
+//   static canDownload = true;
+//   static numOfDevices = 1;
+//   static hasSD = true;
+//   static hasHD = false;
+//   static hasUHD = false;
+//   static price = "$8.99";
+// }
+
+// class StandardPlan extends BasicPlan {
+//   static hasHD = true;
+//   static numOfDevices = 2;
+//   static price = "$12.99";
+// }
+
+// class PremiumPlan extends StandardPlan {
+//   static hasUHD = true;
+//   static numOfDevices = 4;
+//   static price = "$15.99";
+// }
+
+// console.log(PremiumPlan.price);
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 19 - October - 2020
 // Source: https://edabit.com/challenge/bcM99TYJmRv5JnZaA
 // Title: Find the Bug: Checking Even Numbers
 
