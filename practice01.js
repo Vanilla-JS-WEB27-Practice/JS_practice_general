75467f65c80fd35d0333243eaf4d0416d0adf70d
seanaleid <seanaleid@gmail.com>
2020-10-25 22:14:52 -0400

    Reverse Coding Challenge #3

--- practice01.js
+++ practice01.js
@@ -20,6 +20,41 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 25 - October - 2020
+// Source: https://edabit.com/challenge/bADsAhtb5g6pu5WYM
+// Title: Reverse Coding Challenge #3
+
+// Instructions:
+// This is a reverse coding challenge. Normally you're given explicit directions with how to create a function.
+// Here, you must generate your own function to satisfy the relationship between the inputs and outputs.
+// Your task is to create a function that, when fed the inputs below, produce the sample outputs shown.
+
+// Examples:
+// [5, 7, 8, 2, 1], 2 ➞ [1, 1, 0, 0, 1]
+// [9, 8, 16, 47], 4 ➞ [1, 0, 0, 3]
+// [17, 11, 99, 55, 23, 1], 5 ➞ [2, 1, 4, 0, 3, 1]
+// [6, 1], 7 ➞ [6, 1]
+// [3, 2, 9], 3 ➞ [0, 2, 0]
+// [48, 22, 0, 19, 33, 100], 10 ➞ [8, 2, 0, 9, 3, 0]
+
+// Work:
+// function mysteryFunc(arr, num) {
+//   const result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     result.push(arr[i] % num);
+//   }
+//   return result;
+// }
+
+// console.log(mysteryFunc([5, 7, 8, 2, 1], 2));
+// console.log(mysteryFunc([9, 8, 16, 47], 4));
+// console.log(mysteryFunc([17, 11, 99, 55, 23, 1], 5));
+// console.log(mysteryFunc([6, 1], 7));
+// console.log(mysteryFunc([3, 2, 9], 3));
+// console.log(mysteryFunc([48, 22, 0, 19, 33, 100], 10));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 25 - October - 2020
 // Source: https://edabit.com/challenge/TDpT9tvwJK5uLn98h
 // Title: Check if the Same Case
 
@@ -37,12 +72,12 @@
 //   return str === str.toLowerCase() || str === str.toUpperCase() ? true : false;
 // }
 
-const sameCase = (str) =>
-  str === str.toLowerCase() || str === str.toUpperCase() ? true : false;
-console.log(sameCase("hello"));
-console.log(sameCase("HELLO"));
-console.log(sameCase("Hello"));
-console.log(sameCase("ketcHUp"));
+// const sameCase = (str) =>
+//   str === str.toLowerCase() || str === str.toUpperCase() ? true : false;
+// console.log(sameCase("hello"));
+// console.log(sameCase("HELLO"));
+// console.log(sameCase("Hello"));
+// console.log(sameCase("ketcHUp"));
 
 // ------------------------------------------------------------------------------------------------------------------
 
