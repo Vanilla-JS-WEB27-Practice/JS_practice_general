55cd10a503717b7db52431639c3cf51a3fa2b426
seanaleid <seanaleid@gmail.com>
2020-10-25 21:50:27 -0400

    Strange Pair

--- practice01.js
+++ practice01.js
@@ -20,6 +20,45 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 25 - October - 2020
+// Source: https://edabit.com/challenge/WBdjg7zgiqi6L5zsL
+// Title: Strange Pair
+
+// Instructions:
+// A pair of strings form a strange pair if both of the following are true:
+// The 1st string's first letter = 2nd string's last letter.
+// The 1st string's last letter = 2nd string's first letter.
+// Create a function that returns true if a pair of strings constitutes a strange pair, and false otherwise.
+
+// Examples:
+// isStrangePair("ratio", "orator") ➞ true
+// "ratio" ends with "o" and "orator" starts with "o".
+// "ratio" starts with "r" and "orator" ends with "r".
+// isStrangePair("sparkling", "groups") ➞ true
+// isStrangePair("bush", "hubris") ➞ false
+// isStrangePair("", "") ➞ true
+
+// Notes:
+// It should work on a pair of empty strings (they trivially share nothing).
+
+// Work:
+// function isStrangePair(str1, str2) {
+//     if(str1.length === "" && str2.length === ""){
+//         return true;
+//     } else if(str1[0] === str2[str2.length-1] && str2[0] === str1[str1.length-1]){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// console.log(isStrangePair("ratio", "orator"));
+// console.log(isStrangePair("sparkling", "groups"));
+// console.log(isStrangePair("bush", "hubris"));
+// console.log(isStrangePair("", ""));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 25 - October - 2020
 // Source: https://edabit.com/challenge/3Efavz8YmSBia4p8s
 // Title: Index Multiplier
 
