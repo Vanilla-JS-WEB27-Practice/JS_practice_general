a23fa33c528b49d8c0b0e1c58b7c12cf3d4f721a
seanaleid <seanaleid@gmail.com>
2020-10-26 23:05:00 -0400

    Hurdle Jump

--- practice01.js
+++ practice01.js
@@ -20,6 +20,35 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 26 - October - 2020
+// Source: https://edabit.com/challenge/Hp9FitGXpKqdCW6Fd
+// Title: Hurdle Jump
+// Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+// Instructions:
+// Create a function that takes an array of hurdle heights and a jumper's jump height,
+// and determine whether or not the hurdler can clear all the hurdles.
+// A hurdler can clear a hurdle if their jump height is greater than or equal to the hurdle height.
+
+// Examples:
+// hurdleJump([1, 2, 3, 4, 5], 5) ➞ true
+// hurdleJump([5, 5, 3, 4, 5], 3) ➞ false
+// hurdleJump([5, 4, 5, 6], 10) ➞ true
+// hurdleJump([1, 2, 1], 1) ➞ false
+
+// Notes:
+// Return true for the edge case of an empty array of hurdles. (Zero hurdles means that any jump height can clear them).
+
+// Work:
+// const hurdleJump = (hurdles, jumpHeight) => hurdles.every( lessThan = (cv) => cv <= jumpHeight);
+
+// console.log(hurdleJump([1, 2, 3, 4, 5], 5));
+// console.log(hurdleJump([5, 5, 3, 4, 5], 3));
+// console.log(hurdleJump([5, 4, 5, 6], 10));
+// console.log(hurdleJump([1, 2, 1], 1));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 26 - October - 2020
 // Source: https://edabit.com/challenge/wBAuop24JYt9MZhXF
 // Title: Regex Series: String Contains at Least One Digit
 
