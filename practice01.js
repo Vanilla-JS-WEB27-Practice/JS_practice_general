b31646ece7390ac1bf0b818d059a9d115bc9d0f6
seanaleid <seanaleid@gmail.com>
2020-10-29 00:21:34 -0400

    Calculate the Profit

--- practice01.js
+++ practice01.js
@@ -20,6 +20,53 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 29 - October - 2020
+// Source: https://edabit.com/challenge/GJn7xcBiCLdCNXFgy
+// Title: Calculate the Profit
+
+// Instructions:
+// You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product.
+// You are given an object containing the cost price per unit (in dollars), sell price per unit (in dollars), and the starting inventory.
+// Return the total profit made, rounded to the nearest dollar.
+
+// Examples:
+// profit({
+//   costPrice: 32.67,
+//   sellPrice: 45.00,
+//   inventory: 1200
+// }) ➞ 14796
+// profit({
+//   costPrice: 225.89,
+//   sellPrice: 550.00,
+//   inventory: 100
+// }) ➞ 32411
+// profit({
+//   costPrice: 2.77,
+//   sellPrice: 7.95,
+//   inventory: 8500
+// }) ➞ 44030
+
+// Notes:
+// Assume all inventory has been sold.
+// Profit = Total Sales - Total Cost
+
+// Work:
+// function profit(info){
+//   return (info.sellPrice * info.inventory) - (info.costPrice * info.inventory);
+// return info
+// }
+// const profit = (info) =>
+//   Math.round(info.sellPrice * info.inventory - info.costPrice * info.inventory);
+// console.log(
+//   profit({
+//     costPrice: 2.77,
+//     sellPrice: 7.95,
+//     inventory: 8500,
+//   })
+// );
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 29 - October - 2020
 // Source: https://edabit.com/challenge/NfBqxaDu4KoxAysWF
 // Title: Word Endings
 
