48ef4491633827651a461fdda4f00162cefa62d4
seanaleid <seanaleid@gmail.com>
2020-11-06 21:30:25 -0500

    Concatenate to Form Target Array

--- practice01.js
+++ practice01.js
@@ -19,6 +19,72 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 06 - November - 2020
+// Source: https://edabit.com/challenge/f3Lc7Z5dK3p8dahoz
+// Title: Concatenate to Form Target Array
+// Resources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+
+// Instructions:
+// Create a function that returns true if smaller arrays can concatenate to form the target array and false otherwise.
+
+// Examples:
+// canConcatenate([[1, 2, 3, 4], [5, 6], [7]], [1, 2, 3, 4, 5, 6, 7]) ➞ true
+// canConcatenate([[2, 1, 3], [5, 4, 7, 6]], [7, 6, 5, 4, 3, 2, 1]) ➞ true
+// canConcatenate([[2, 1, 3], [5, 4, 7, 6, 7]], [1, 2, 3, 4, 5, 6, 7]) ➞ false
+// Duplicate 7s not found in target array.
+// canConcatenate([[2, 1, 3], [5, 4, 7]], [1, 2, 3, 4, 5, 6, 7]) ➞ false
+// Missing 6 from target array.
+
+// Notes:
+// Arrays do not have to be sorted (see example #2).
+// Arrays should concatenate to create the final array exactly (see examples #3 and #4).
+
+// Work:
+// function canConcatenate(arr, target) {
+//   Option 1
+//   const result = [];
+//   arr.map((x) => result.push(...x));
+//   return target.length === result.length ;
+
+//   Option 2
+//   const result = arr.flat();
+//   return target.length === arr.flat().length;
+// }
+
+//   Option 3
+// const canConcatenate = (arr, target) => target.length === arr.flat().length;
+
+// console.log(canConcatenate([[1, 2, 3, 4], [5, 6], [7]], [1, 2, 3, 4, 5, 6, 7]));
+// console.log(
+//   canConcatenate(
+//     [
+//       [2, 1, 3],
+//       [5, 4, 7, 6],
+//     ],
+//     [7, 6, 5, 4, 3, 2, 1]
+//   )
+// );
+// console.log(
+//   canConcatenate(
+//     [
+//       [2, 1, 3],
+//       [5, 4, 7, 6, 7],
+//     ],
+//     [1, 2, 3, 4, 5, 6, 7]
+//   )
+// );
+// console.log(
+//   canConcatenate(
+//     [
+//       [2, 1, 3],
+//       [5, 4, 7],
+//     ],
+//     [1, 2, 3, 4, 5, 6, 7]
+//   )
+// );
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 05 - November - 2020
 // Source: https://edabit.com/challenge/YjkNx49pk7Eois9pi
 // Title: Broken Bridge
