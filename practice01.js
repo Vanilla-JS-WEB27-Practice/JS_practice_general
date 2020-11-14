dae2d804e7c46926667064ae59f6d8ad3482cd84
seanaleid <seanaleid@gmail.com>
2020-11-13 22:48:04 -0500

    Leader in an Array

--- practice01.js
+++ practice01.js
@@ -19,6 +19,49 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 13 - November - 2020
+// Source: https://edabit.com/challenge/wYdQGudMRdzME5B7m
+// Title: Leader in an Array
+
+// Instructions:
+// Create a function that finds each number in the given array that is greater than every number that comes after it.
+// Your solution should return an array of the number(s) that meet these criteria.
+
+// Examples:
+// leader([2, 3, 20, 15, 8, 3]) ➞ [20, 15, 8, 3]
+// Note that 20 is greater than all the elements to it's
+// right side, similarly 15 and so on.
+// leader([2, 3, 20, 15, 8, 25, 3]) ➞ [25, 3]
+// Note that 20 cannot be added because it is not greater than 25.
+// leader([1, 2, 3, 4, 5]) ➞ [5]
+// 5 is technically greater than the (zero) remaining items.
+// leader([8, 7, 1, 2, 10, 3, 5]) ➞[10, 5]
+// 10 is greater than all items to the right of it, so include.
+// 3 is not greater than all items to the right of it, so exclude.
+// 5 is greater than the remaining items, so include.
+
+// Notes:
+// Add elements in the new array in the same order they occur in the input array.
+
+// Work:
+// function leader(arr) {
+//   const result = [];
+//   const test = arr.indexOf(Math.max(...arr));
+//   for (let i = test; i < arr.length; i++){
+//     if (arr[i] === arr[test] || i === arr.length-1 || arr[i] > arr[i+1]){
+//       result.push(arr[i])
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(leader([2, 3, 20, 15, 8, 3]));
+// console.log(leader([2, 3, 20, 15, 8, 25, 3]));
+// console.log(leader([1, 2, 3, 4, 5]));
+// console.log(leader([8, 7, 1, 2, 10, 3, 5]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 08 - November - 2020
 // Source: https://edabit.com/challenge/LHTF9ij2FtvwhnzQF
 // Title: Array from Comma-Delimited String
