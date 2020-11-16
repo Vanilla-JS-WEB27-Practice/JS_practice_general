c3643e202828f0a8877b0fc5fb7a280fe574887c
seanaleid <seanaleid@gmail.com>
2020-11-15 22:17:17 -0500

    Word Endings

--- practice01.js
+++ practice01.js
@@ -20,6 +20,36 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 15 - November - 2020
+// Source: https://edabit.com/challenge/hwWLHcDoPZmXcGHyd
+// Title: Word Endings
+
+// Instructions:
+// Create a function that adds a string ending to each member in an array.
+
+// Examples:
+// addEnding(["clever", "meek", "hurried", "nice"], "ly")
+// ➞ ["cleverly", "meekly", "hurriedly", "nicely"]
+// addEnding(["new", "pander", "scoop"], "er")
+// ➞ ["newer", "panderer", "scooper"]
+// addEnding(["bend", "sharpen", "mean"], "ing")
+// ➞ ["bending", "sharpening", "meaning"]
+
+// Notes:
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// Work:
+
+const addEnding = (arr, ending) => arr.map((x) => x.concat(ending));
+
+console.log(addEnding(["clever", "meek", "hurried", "nice"], "ly"));
+console.log(addEnding(["new", "pander", "scoop"], "er"));
+console.log(addEnding(["bend", "sharpen", "mean"], "ing"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 15 - November - 2020
 // Source: https://edabit.com/challenge/YjkNx49pk7Eois9pi
 // Title: Broken Bridge
 
