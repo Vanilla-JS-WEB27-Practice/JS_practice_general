dffdae592ece4b4ddd2dcc7257eeec37c736ce52
seanaleid <seanaleid@gmail.com>
2020-11-16 22:59:31 -0500

    Number Length Sort

--- practice01.js
+++ practice01.js
@@ -20,22 +20,32 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 16 - November - 2020
-// Source:
-// Title:
+// Source: https://edabit.com/challenge/fuvMP8KMtiQY3QXrJ
+// Title: Number Length Sort
+// Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort --> description for the ternary 1 : -1
 
 // Instructions:
-//
+// Create a sorting function which sorts numbers not by numerical order, but by number length!
+// This means sorting numbers with the least amount of digits first, up to the numbers with the most digits.
 
 // Examples:
-//
+// numberLenSort([1, 54, 1, 2, 463, 2]) ➞ [1, 1, 2, 2, 54, 463]
+// numberLenSort([999, 421, 22, 990, 32]) ➞ [22, 32, 999, 421, 990]
+// numberLenSort([9, 8, 7, 6, 5, 4, 31, 2, 1, 3]) ➞ [9, 8, 7, 6, 5, 4, 2, 1, 3, 31]
 
 // Notes:
-//
-
-// Sudo code:
-// 1 -
+// If two numbers have the same number of digits, return them in the order they first appeared (this makes it different to just sorting the numbers normally).
 
 // Work:
+// function numberLenSort(arr) {
+//   return arr.sort((a, b) => a.toString().length >= b.toString().length ? 1 : -1);
+// }
+
+// const numberLenSort = (arr) => arr.sort((a, b) => (a.toString().length >= b.toString().length ? 1 : -1));
+
+// console.log(numberLenSort([1, 54, 1, 2, 463, 2]));
+// console.log(numberLenSort([999, 421, 22, 990, 32]));
+// console.log(numberLenSort([9, 8, 7, 6, 5, 4, 31, 2, 1, 3]));
 
 // ------------------------------------------------------------------------------------------------------------------
 
