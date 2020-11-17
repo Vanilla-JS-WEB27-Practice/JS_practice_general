feac5b1071238795b6c4aae86a7536b1e12bc425
seanaleid <seanaleid@gmail.com>
2020-11-16 22:46:30 -0500

    Fix the Error: Flattening an Array

--- practice01.js
+++ practice01.js
@@ -19,6 +19,83 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 16 - November - 2020
+// Source:
+// Title:
+
+// Instructions:
+//
+
+// Examples:
+//
+
+// Notes:
+//
+
+// Sudo code:
+// 1 -
+
+// Work:
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Template
+// Date: 16 - November - 2020
+// Source: https://edabit.com/challenge/MXChCnzCSBobgY4Ex
+// Title: Fix the Error: Flattening an Array
+
+// Instructions:
+// I'm trying to write a function to flatten an array of subarrays into one array.
+// (Suppose I am unware there is a .flat() method in the Array prototype).
+// In other words, I want to transform this: [[1, 2], [3, 4]] into [1, 2, 3, 4].
+// Here is my code:
+// function flatten(arr) {
+//   arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     arr2.concat(arr[i]);
+//   }
+//   return arr2;
+// }
+// But...it doesn't seem to be working! Fix my code so that it correctly flattens the array.
+
+// Examples:
+// flatten([[1, 2], [3, 4]]) ➞ []
+// Expected: [1, 2, 3, 4]
+// flatten([["a", "b"], ["c", "d"]]) ➞ []
+// Expected: ["a", "b", "c", "d"]
+// flatten([[true, false], [false, false]]) ➞ []
+// Expected: [true, false, false, false]
+
+// Work:
+// function flatten(arr) {
+//   arr2 = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     arr2.push(...arr[i]);
+//   }
+//   return arr2;
+// }
+
+// console.log(
+//   flatten([
+//     [1, 2],
+//     [3, 4],
+//   ])
+// );
+// console.log(
+//   flatten([
+//     ["a", "b"],
+//     ["c", "d"],
+//   ])
+// );
+// console.log(
+//   flatten([
+//     [true, false],
+//     [false, false],
+//   ])
+// );
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 15 - November - 2020
 // Source: https://edabit.com/challenge/KQQ7RMXjSK257GQgy
 // Title: Re-Form the Word
