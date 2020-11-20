e3ce90c395e785ae2b6279f0f2655a015b93d118
seanaleid <seanaleid@gmail.com>
2020-11-19 22:56:05 -0500

    Bundle Up

--- practice01.js
+++ practice01.js
@@ -19,6 +19,52 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 19 - November - 2020
+// Source: https://edabit.com/challenge/saKcrsreCGXD28hgE
+// Title: Bundle Up!
+
+// Instructions:
+// Lets assume for the purposes of this challenge that for every layer of fabric you wear when it's cold outside
+// (coats, cardigans, etc), the temperature increases by a tenth of the total.
+// Given n number of layers and a given temperature, return the temperature inside of all those warm fuzzy layers. Round to the nearest tenth of a degree.
+// calcBundledTemp(2, "10*C") ➞ "12.1*C"
+// 10 * 1.1 = 11
+// 11 * 1.1 = 12.1
+
+// Examples:
+// calcBundledTemp(1, "2*C") ➞ "2.2*C"
+// calcBundledTemp(4, "6*C") ➞ "8.8*C"
+// calcBundledTemp(20, "4*C") ➞ "26.9*C"
+
+// Notes:
+// The temperature will be given in celsius and as a string.
+// Note that the degrees sign is given as an asterisk.
+
+// Work:
+// function calcBundledTemp(n, temp) {
+//   const deg = temp.split("");
+//   deg.shift();
+//   let finalTemp = temp.split("*")[0];
+//   for (let i = 0; i < n; i++) {
+//     finalTemp *= 1.1;
+//   }
+//   return `${finalTemp.toFixed(1)}*C`;
+// }
+
+// console.log(calcBundledTemp(2, "10*C"));
+// console.log(calcBundledTemp(1, "2*C"));
+// console.log(calcBundledTemp(4, "6*C"));
+// console.log(calcBundledTemp(20, "4*C"));
+// console.log(calcBundledTemp(5, "20*C"));
+// console.log(calcBundledTemp(20, "3*C"));
+// console.log(calcBundledTemp(5, "18*C"));
+// console.log(calcBundledTemp(4, "5*C"));
+// console.log(calcBundledTemp(16, "17*C"));
+// console.log(calcBundledTemp(15, "2*C"));
+// console.log(calcBundledTemp(14, "10*C"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 17 - November - 2020
 // Source: https://edabit.com/challenge/zRCyxKBBmr4F2x4Bv
 // Title: Find the nth Tetrahedral Number
