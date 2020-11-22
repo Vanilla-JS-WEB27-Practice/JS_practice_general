3048a809a85b2ad22f8bd6335a4df12f53ed32d2
seanaleid <seanaleid@gmail.com>
2020-11-21 23:55:22 -0500

    Temperature Conversion

--- practice01.js
+++ practice01.js
@@ -19,6 +19,38 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 21 - November - 2020
+// Source: https://edabit.com/challenge/QW5CApff3WAGszrWY
+// Title: Temperature Conversion
+
+// Instructions:
+// Write a program that takes a temperature input in celsius and converts it to Fahrenheit and Kelvin. Return the converted temperature values in an array.
+// The formula to calculate the temperature in Fahrenheit from Celsius is:
+// F = C*9/5 +32
+// The formula to calculate the temperature in Kelvin from Celsius is:
+// K = C + 273.15
+
+// Examples:
+// tempConversion(0) ➞ [32, 273.15]
+// 0°C is equal to 32°F and 273.15 K.
+// tempConversion(100) ➞ [212, 373.15]
+// tempConversion(-10) ➞ [14, 263.15]
+// tempConversion(300.4) ➞ [572.72, 573.55]
+
+// Notes:
+// Return calculated temperatures up to two decimal places.
+// Return "Invalid" if K is less than 0.
+
+// Work:\
+// const tempConversion = (celsius) => Number((celsius + 273.15).toFixed(2)) < 0 ? "Invalid" : [Number((celsius * 1.8 + 32).toFixed(2)), Number((celsius + 273.15).toFixed(2))];
+
+// console.log(tempConversion(100));
+// console.log(tempConversion(-10));
+// console.log(tempConversion(300.4));
+// console.log(tempConversion(0));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 19 - November - 2020
 // Source: https://edabit.com/challenge/uRtubbXsDHLwAz5RS
 // Title: Smaller String Number
