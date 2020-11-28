94632f592fdca93fd0114c86129bb489d1a718c3
seanaleid <seanaleid@gmail.com>
2020-11-27 22:58:07 -0500

    Remove the Letters ABC

--- practice01.js
+++ practice01.js
@@ -19,6 +19,41 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 27 - November - 2020
+// Source: https://edabit.com/challenge/RftQjZs3srJnRBEH8
+// Title: Remove the Letters ABC
+
+// Instructions:
+// Create a function that will remove the letters "a", "b" and "c" from the given string and return the modified version.
+// If the given string does not contain "a", "b", or "c", return null.
+
+// Examples:
+// removeABC("This might be a bit hard") ➞ "This might e  it hrd"
+// removeABC("hello world!") ➞ null
+// removeABC("") ➞ null
+
+// function removeABC(str) {
+//   const regex = /[a-c]/gi;
+//   return str.replace(regex, "");
+// }
+
+// my short solution
+// const removeABC = (str) => str.match(/[a-c]/gi) ? str.replace(/[a-c]/gi, "") : null;
+
+// another coder's solution
+// function removeABC(s) {
+// 	r=/[abc]/g
+// 	return r.test(s)?s.replace(r,''):null
+// }
+
+// console.log(removeABC("This might be a bit hard"));
+// console.log(removeABC("hello world!"));
+// console.log(removeABC(""));
+
+// Work:
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 24 - November - 2020
 // Source: https://edabit.com/challenge/wv6p5WR648oG6mTZx
 // Title: Integer Digits Count
