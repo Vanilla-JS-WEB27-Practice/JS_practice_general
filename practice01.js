666f6e4e648a4ee81218b1d7dcd6427149157d79
seanaleid <seanaleid@gmail.com>
2020-12-08 12:36:23 -0500

    Check If the Brick Fits through the Hole

--- practice01.js
+++ practice01.js
@@ -19,6 +19,44 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 08 - December - 2020
+// Source: https://edabit.com/challenge/ixdXLyopP7c4aPXqx
+// Title: Check If the Brick Fits through the Hole
+
+// Instructions:
+// Write the function that takes three dimensions of a brick:
+// height(a), width(b) and depth(c)
+// and returns true if this brick can fit into a hole with the width(w) and height(h).
+
+// Examples:
+// doesBrickFit(1, 1, 1, 1, 1) ➞ true
+// doesBrickFit(1, 2, 1, 1, 1) ➞ true
+// doesBrickFit(1, 2, 2, 1, 1) ➞ false
+
+// Notes:
+// You can turn the brick with any side towards the hole.
+// We assume that the brick fits if its sizes equal the ones of the hole
+// (i.e. brick size should be less than or equal to the size of the hole, not strictly less).
+// You can't put a brick in at a non-orthogonal angle.
+
+// Work:
+// version three - mine
+// const doesBrickFit = (a, b, c, w, h) => a * (c || b) <= w * h;
+
+// version two - mine
+// const doesBrickFit = (a, b, c, w, h) => a * c <= w * h || a * b <= w * h;
+
+// version one - mine
+// function doesBrickFit(a, b, c, w, h) {
+//   return a * c <= w * h || a * b <= w * h;
+// }
+
+// console.log(doesBrickFit(1, 1, 1, 1, 1));
+// console.log(doesBrickFit(1, 2, 1, 1, 1));
+// console.log(doesBrickFit(1, 2, 2, 1, 1));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 07 - December - 2020
 // Source: https://edabit.com/challenge/rvsvGvqZ3BzNieKqA
 // Title: What's Hiding Amongst the Crowd?
