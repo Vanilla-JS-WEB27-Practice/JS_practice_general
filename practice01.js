6d8d8100cf6cfa38ea42ce20ba8feb1e34fbe55f
seanaleid <seanaleid@gmail.com>
2020-12-07 22:29:50 -0500

    The Frugal Gentleman

--- practice01.js
+++ practice01.js
@@ -19,6 +19,67 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 07 - December - 2020
+// Source: https://edabit.com/challenge/ujzhzyvGoASKxSAib
+// Title: The Frugal Gentleman
+
+// Instructions:
+// Atticus has been invited to a dinner party, and he decides to purchase a bottle of wine.
+// However, he has little knowledge of how to choose a good bottle.
+// Being a very frugal gentleman (yet disliking looking like a cheapskate), he decides to use a very simple rule.
+// In any selection of two or more wines, he will always buy the second-cheapest.
+// Given an array of wine objects, write a function that returns the name of the wine he will buy for the party.
+// If given an empty array, return null. If given an array of only one, Atticus will buy that wine.
+
+// Examples:
+// chosenWine([
+//   { name: "Wine A", price: 8.99 },
+//   { name: "Wine 32", price: 13.99 },
+//   { name: "Wine 9", price: 10.99 }
+// ]) ➞ "Wine 9"
+
+// chosenWine([{ name: "Wine A", price: 8.99 }]) ➞ "Wine A"
+
+// chosenWine([]) ➞ null
+
+// Notes:
+// All wines will be different prices, so there is no confusion in the ordering.
+
+// Sudo code:
+// 1 - write initial conditional to check for an empty array --> return null
+// 2 - write next conditioanl to check for length of 1 --> return name
+// 3 - write last conditional to check more length greater than 1 --> return second to last priced wine name
+
+// Work:
+// second solution - mine
+// const chosenWine = (wines) => !wines.length ? null : wines.length === 1 ? wines[0].name : wines.sort((a, b) => a.price - b.price)[1].name;
+
+// first solution - mine
+// function chosenWine(wines) {
+//   if (!wines.length) {
+//     return null;
+//   } else if (wines.length === 1) {
+//     return wines[0].name;
+//   } else if (wines.length > 1) {
+//     const sortedWines = wines.sort((a, b) => a.price - b.price);
+//     return sortedWines[1].name;
+//   }
+// }
+
+// console.log(
+//   chosenWine([
+//     { name: "Wine A", price: 8.99 },
+//     { name: "Wine 32", price: 13.99 },
+//     { name: "Wine 9", price: 10.99 },
+//   ])
+// );
+
+// console.log(chosenWine([{ name: "Wine A", price: 8.99 }]));
+
+// console.log(chosenWine([]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 02 - December - 2020
 // Source: https://edabit.com/challenge/7ysTEDruHz2prcJQ9
 // Title: Tuck in Array
