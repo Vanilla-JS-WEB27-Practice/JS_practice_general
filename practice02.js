314d9ee2c810dd1c76ccc45ec5da76fb2c1df8b1
seanaleid <seanaleid@gmail.com>
2021-01-06 23:25:18 -0500

    Check if All Values Are True

--- practice02.js
+++ practice02.js
@@ -19,6 +19,31 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 6 - January - 2021
+// Source: https://edabit.com/challenge/ttiutYw6NyphfxuCG
+// Title: Check if All Values Are True
+
+// Instructions:
+// Create a function that returns true if all parameters are truthy, and false otherwise.
+
+// Examples:
+// allTruthy(true, true, true) ➞ true
+// allTruthy(true, false, true) ➞ false
+// allTruthy(5, 4, 3, 2, 1, 0) ➞ false
+
+// Notes:
+// Falsy values include false, 0, "" (empty string), null, undefined, and NaN; everything else is truthy.
+// You will always be supplied with at least one parameter.
+
+// Work:
+// const allTruthy = (...args) => [...args].every(x => Boolean(x))
+
+// console.log(allTruthy(true, true, true));
+// console.log(allTruthy(true, false, true));
+// console.log(allTruthy(5, 4, 3, 2, 1, 0));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 02 - January - 2021
 // Source: https://edabit.com/challenge/zptHytXMgKcAjr4TH
 // Title: East or West
