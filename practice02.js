a0d01f541ecbf490beb7927195f2f35a27037cb2
seanaleid <seanaleid@gmail.com>
2021-01-18 22:53:37 -0500

    Capitalize the Names

--- practice02.js
+++ practice02.js
@@ -19,6 +19,36 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 18 - January - 2021
+// Source: https://edabit.com/challenge/ZN5cpGPNRS3nrjMo5
+// Title: Capitalize the Names
+
+// Instructions:
+// Create a function that takes an array of names and returns an array where only the first letter of each name is capitalized.
+
+// Examples:
+// capMe(["mavis", "senaida", "letty"]) ➞ ["Mavis", "Senaida", "Letty"]
+// capMe(["samuel", "MABELLE", "letitia", "meridith"]) ➞ ["Samuel", "Mabelle", "Letitia", "Meridith"]
+// capMe(["Slyvia", "Kristal", "Sharilyn", "Calista"]) ➞ ["Slyvia", "Kristal", "Sharilyn", "Calista"]
+
+// Notes:
+// Don't change the order of the original array.
+// Notice in the second example above, "MABELLE" is returned as "Mabelle".
+
+// Work:
+// const capMe = (a) => a.map(x => `${x.substring(0,1).toUpperCase()}${x.substring(1).toLowerCase()}`);
+
+// first pass
+// function capMe(arr){
+//   return arr.map(x => `${x.substring(0,1).toUpperCase()}${x.substring(1).toLowerCase()}`);
+// }
+
+// console.log(capMe(["mavis", "senaida", "letty"]));
+// console.log(capMe(["samuel", "MABELLE", "letitia", "meridith"]));
+// console.log(capMe(["Slyvia", "Kristal", "Sharilyn", "Calista"]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 15 - January - 2021
 // Source: https://edabit.com/challenge/8NDcdD8QZiMKJJaYL
 // Title: True Alphabetical Order
