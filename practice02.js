6ea05e13f673e37fab865ff74c0b4afaf078b7c1
seanaleid <seanaleid@gmail.com>
2021-01-24 23:32:56 -0500

    Reversing a Binary String

--- practice02.js
+++ practice02.js
@@ -19,6 +19,36 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 24 - January - 2021
+// Source: https://edabit.com/challenge/4s93F8ZiEdHjmMnMv
+// Title: Reversing a Binary String
+
+// Instructions:
+// Write a function that takes an integer n, reverses the binary representation of that integer, and returns the new integer from the reversed binary.
+
+// Examples:
+// reversedBinaryInteger(10) ➞ 5
+// 10 = 1010 -> 0101 = 5
+// reversedBinaryInteger(12) ➞ 3
+// 12 = 1100 -> 0011 = 3
+// reversedBinaryInteger(25) ➞ 19
+// 25 = 11001 -> 10011 = 19
+// reversedBinaryInteger(45) ➞ 45
+// 45 = 101101 -> 101101 = 45
+
+// Notes:
+// All values of n will be positive.
+
+// Work:
+const reversedBinaryInteger = (n) => parseInt(n.toString(2).split("").reverse().join(""), 2);
+
+console.log(reversedBinaryInteger(10));
+console.log(reversedBinaryInteger(12));
+console.log(reversedBinaryInteger(25));
+console.log(reversedBinaryInteger(45));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 23 - January - 2021
 // Source: https://edabit.com/challenge/257hD8dzvsyR4G839
 // Title: Sum of Missing Numbers
