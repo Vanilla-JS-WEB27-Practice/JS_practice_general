d607e69eea316ddb00ace30935129dff8cd22e2e
seanaleid <seanaleid@gmail.com>
2021-01-25 19:07:00 -0500

    Length of a Nested Array

--- practice02.js
+++ practice02.js
@@ -19,6 +19,76 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 25 - January - 2021
+// Source: https://edabit.com/challenge/yXSTvCNen2DQHyrh6
+// Title: Length of a Nested Array
+// Resource: https://gist.github.com/dgowrie/ed1b416d4927388915e5
+
+// Instructions:
+// The .length property on an array will return the number of elements in the array. For example, the array below contains 2 elements:
+// [1, [2, 3]]
+// 2 elements, number 1 and array [2, 3]
+// Suppose we instead wanted to know the total number of non-nested items in the nested array. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
+// Write a function that returns the total number of non-nested items in a nested array.
+
+// Examples:
+// getLength([1, [2, 3]]) ➞ 3
+
+// getLength([1, [2, [3, 4]]]) ➞ 4
+
+// getLength([1, [2, [3, [4, [5, 6]]]]]) ➞ 6
+
+// getLength([1, [2], 1, [2], 1]) ➞ 5
+
+// Notes:
+// An empty array should return 0.
+
+// Work:
+
+// another option from user(Peter Flynn) => easier, longer way to read a recursive example
+// const getLength = a => {
+//   let count = 0;
+//   a.forEach(x => {
+//     if(Array.isArray(x)){
+//       count += getLength(x);
+//     } else {
+//       count++
+//     }
+//   })
+//   return count
+// }
+
+// another option from another user(feathers) => way easier and MUCH MUCH shorter!! 
+// const getLength = a => a.flat(Infinity).length
+
+// solution modified form resource
+// const getLength = (a) => {
+//   const flat = [];
+//   function flatten(nested){
+//     for(let i = 0; i < nested.length; i++){
+//       if(isArray(nested[i])){
+//         console.log('nested', nested[i]);
+//         flatten(nested[i])
+//       } else {
+//         flat.push(nested[i])
+//       }
+//     }
+//     return flat.length;
+//   }
+//   function isArray(a){
+//     return a instanceof Array
+//   }
+//   return flatten(a)
+// }
+
+// console.log(getLength([1, [2, 3]]));
+// console.log(getLength([1, [2, [3, 4]]]));
+// console.log(getLength([1, [2, [3, [4, [5, 6]]]]]));
+// console.log(getLength([1, [2], 1, [2], 1]));
+// console.log(getLength([]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 24 - January - 2021
 // Source: https://edabit.com/challenge/4s93F8ZiEdHjmMnMv
 // Title: Reversing a Binary String
@@ -40,12 +110,12 @@
 // All values of n will be positive.
 
 // Work:
-const reversedBinaryInteger = (n) => parseInt(n.toString(2).split("").reverse().join(""), 2);
+// const reversedBinaryInteger = (n) => parseInt(n.toString(2).split("").reverse().join(""), 2);
 
-console.log(reversedBinaryInteger(10));
-console.log(reversedBinaryInteger(12));
-console.log(reversedBinaryInteger(25));
-console.log(reversedBinaryInteger(45));
+// console.log(reversedBinaryInteger(10));
+// console.log(reversedBinaryInteger(12));
+// console.log(reversedBinaryInteger(25));
+// console.log(reversedBinaryInteger(45));
 
 // ------------------------------------------------------------------------------------------------------------------
 
