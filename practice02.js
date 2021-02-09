9c6b38bd41510483b14f72b4c6e05ceb4e2a6a25
seanaleid <seanaleid@gmail.com>
2021-02-08 22:44:49 -0500

    Calculate an Earned Run Average

--- practice02.js
+++ practice02.js
@@ -20,6 +20,33 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 08 - February - 2021
+// Source: https://edabit.com/challenge/94RHwrv6PfaqEGYtr
+// Title: Calculate an Earned Run Average
+
+// Instructions:
+// Create a function that returns an Earned Run Average (ERA). An ERA is calculated by multiplying 9 by the quotient of Earned Runs Allowed er divided by ip Innings Pitched.
+// In baseball statistics, innings are represented with a fractional part of .1 (1/3) or .2 (2/3) to represent the number of outs in an inning. 
+// A whole number or a number with a fractional part of .0 represents a full inning with three outs. Check the Resources tab for a deeper explanation.
+
+// Examples:
+// era(22, 99) ➞ 2.00
+// era(23, 99.1) ➞ 2.08
+// era(24, 99.2) ➞ 2.17
+
+// Notes:
+// ERA is represented with a scale of 2: 2.08
+// For 1/3 and 2/3, use a scale of 2.
+
+// Work:
+// const era = (er, ip) => Number(((er/(ip+1/3))*9).toFixed(2)) < 2.00 ? '2.00' : ((er/(ip+1/3))*9).toFixed(2);
+
+// console.log(era(22, 99));
+// console.log(era(23, 99.1));
+// console.log(era(24, 99.2));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 08 - February - 2021
 // Source: https://edabit.com/challenge/meoZZBsxboQojBzWC
 // Title: Diagonal of a Cube
 // Resources: https://byjus.com/diagonal-of-a-cube-formula/#:~:text=The%20main%20diagonal%20of%20a,the%20square%20root%20of%203.
