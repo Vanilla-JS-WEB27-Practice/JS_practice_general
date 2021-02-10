30c0edd1cefce2332297abbafc4c8336dd70727f
seanaleid <seanaleid@gmail.com>
2021-02-09 23:40:45 -0500

    How Heavy Is It?

--- practice02.js
+++ practice02.js
@@ -19,6 +19,32 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 09 - February - 2021
+// Source: https://edabit.com/challenge/7pAcMPNur4JcXreHS
+// Title: How Heavy Is It?
+
+// Instructions:
+// Given radius r and height h (in cm), calculate the mass of a cylinder when it's filled with water and the cylinder itself doesn't weigh anything. 
+// The desired output should be given in kg and rounded to two decimal places.
+// How to solve:
+// Calculate the volume of the cylinder.
+// Convert cm³ into dm³.
+// 1dm³ = 1L, 1L is 1Kg.
+
+// Examples:
+// weight(4, 10) ➞ 0.5
+// weight(30, 60) ➞ 169.65
+// weight(15, 10) ➞ 7.07
+
+// Work:
+// const weight = (r,h) => Number(((Math.PI*r**2*h)/1000).toFixed(2));
+
+// console.log(weight(4,10));
+// console.log(weight(30,60));
+// console.log(weight(15,10));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 08 - February - 2021
 // Source: https://edabit.com/challenge/wQGGp6Qce6phDCvW9
 // Title: Do All Bigrams Exist?
