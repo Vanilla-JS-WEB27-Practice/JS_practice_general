0fa3244a4e2ad9036d86d3b6ee763416d99c69b0
seanaleid <seanaleid@gmail.com>
2021-02-13 21:57:34 -0500

    Slightly Superior

--- practice02.js
+++ practice02.js
@@ -20,6 +20,46 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 13 - February - 2021
+// Source: https://edabit.com/challenge/xtHTBXjumRg5AhsT5
+// Title: Slightly Superior
+
+// Instructions:
+// You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counterpart 
+// (which means that evaluating the value > the other value will return true).
+// Create a function that returns whether the first array is slightly superior to that of the second.
+// Worked Example
+// isFirstSuperior([1, 2, 4], [1, 2, 3]) ➞ true
+// The pair of items at each index are compared in turn.
+// 1 from the first array is the same as 1 from the second array.
+// 2 is the same as 2.
+// However, 4 is greater than 3, so first array is superior.
+
+// Examples:
+// isFirstSuperior(["a", "d", "c"], ["a", "b", "c"]) ➞ true
+// isFirstSuperior(["zebra", "ostrich", "whale"], ["ant", "ostrich", "whale"]) ➞ true
+// isFirstSuperior([1, 2, 3, 4], [1, 2, 4, 4]) ➞ false
+// isFirstSuperior([true, 10, "zebra"], [true, 10, "zebra"]) ➞ false
+
+// Notes:
+// Both arrays will be the same length.
+// All values and their counterparts will always be of the same data type.
+// If the two arrays are the same, return false.
+
+// Work:
+// solution by cvf on edabit
+// const isFirstSuperior = (arr1, arr2) => arr1 > arr2;
+
+// my solution
+// const isFirstSuperior = (arr1, arr2) => arr1.some((x,i) => x > arr2[i]);
+
+// console.log(isFirstSuperior(['a','d','c'],['a','b','c']));
+// console.log(isFirstSuperior(["zebra", "ostrich", "whale"], ["ant", "ostrich", "whale"]));
+// console.log(isFirstSuperior([1, 2, 3, 4], [1, 2, 4, 4]));
+// console.log(isFirstSuperior([true, 10, "zebra"], [true, 10, "zebra"]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 13 - February - 2021
 // Source: https://edabit.com/challenge/pbK3wzLDvdwfEKgMt
 // Title: Broken Keyboard
 
