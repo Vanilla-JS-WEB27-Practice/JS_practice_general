ef9224477307d05b2151de8e1a23cd1cd94c03e1
seanaleid <seanaleid@gmail.com>
2021-02-13 23:06:51 -0500

    Learn Lodash (4): _.dropRight, Drop the Last Elements of an Array

--- practice02.js
+++ practice02.js
@@ -20,6 +20,38 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 13 - February - 2021
+// Source: https://edabit.com/challenge/LhBwyiMeWiADZhydE
+// Title: Learn Lodash (4): _.dropRight, Drop the Last Elements of an Array
+
+// Instructions:
+// According to the lodash documentation, _.dropRight Creates a slice of an array with n elements dropped from the end.
+// This challenge requires you to write your own version of this function without using lodash so that you can better understand it works.
+
+// Examples:
+// dropRight([1, 2, 3]) ➞ [1, 2]
+// dropRight([1, 2, 3], 2) ➞ [1]
+// dropRight([1, 2, 3], 5) ➞ []
+// dropRight([1, 2, 3], 0) ➞ [1, 2, 3]
+
+// Notes:
+// Do not attempt to import lodash; you are simply writing your own version.
+// This entire series of challenges can be found here.
+
+// Work:
+// solution by Giorgi Dzigoshvili
+// const dropRight = (a, num=1) => a.splice(a,a.length-num)
+
+// my solution
+// const dropRight = (a, num=1) => a.reverse().slice(num).reverse()
+
+// console.log(dropRight([1,2,3]));
+// console.log(dropRight([1,2,3],2));
+// console.log(dropRight([1,2,3],5));
+// console.log(dropRight([1,2,3],0));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 13 - February - 2021
 // Source: https://edabit.com/challenge/xtHTBXjumRg5AhsT5
 // Title: Slightly Superior
 
