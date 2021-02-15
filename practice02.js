53e91a0de65ce2f3eb29f2e3dfb9f3e3af2306e0
seanaleid <seanaleid@gmail.com>
2021-02-14 22:59:49 -0500

    No Good Numbers

--- practice02.js
+++ practice02.js
@@ -19,6 +19,66 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 14 - February - 2021
+// Source: https://edabit.com/challenge/eaf8EaFRPwKNGqwZF
+// Title: No Good Numbers
+
+// Instructions:
+// A positive number's population is the sum of 1's in its binary representation.
+// An evil number has an even numbered population.
+// An odious number has an odd numbered population.
+// A number is pernicious if its population is a prime number.
+// Create a function that takes a number as an argument and returns a sorted array of all its descriptors ("Evil", "Odious", or "Pernicious").
+
+// Examples:
+// howBad(7) ➞ ["Odious", "Pernicious"]
+// 7 in binary is "111".
+// 1 + 1 + 1 = 3 in "111" means "Odious" should be added to the array answer.
+// 3 is a prime number, meaning "Pernicious" should also be added.
+
+// howBad(17) ➞ ["Evil", "Pernicious"]
+// 17 in binary is "10001".
+// 1 + 1 = 2 in "10001" means "Evil" should be added to the array answer.
+// 2 is a prime number, meaning "Pernicious" should also be added.
+
+// howBad(23) ➞ ["Evil"]
+// 23 in binary is "10111".
+// Four 1's in "10111" means "Evil" should be added to the array answer.
+// 4 is not a prime number, meaning "Pernicious" should not be added.
+
+// Work:
+
+// my solution 
+// const howBad = (num) => {
+//   const result = [];
+//   const binary = (num >>> 0).toString(2).split("").reduce((a,b) => Number(a)+Number(b));
+//   const prime = [2,3,5,7,11,13,17,19,23];
+//   if(binary % 2 === 0 ){
+//     result.push("Evil")
+//     if (binary === 2){
+//       result.push("Pernicious")  
+//     }
+//   } else if(binary % 2 !== 0 && prime.indexOf(binary) > -1){
+//     result.push("Odious", "Pernicious")
+//   } else if(binary % 2 !== 0){
+//     result.push("Odious")
+//   }
+//   return result
+// }
+
+// console.log(howBad(7));
+// console.log(howBad(17));
+// console.log(howBad(23));
+// console.log(howBad(97));
+// console.log(howBad(66));
+// console.log(howBad(77));
+// console.log(howBad(128));
+// console.log(howBad(666));
+// console.log(howBad(987654321));
+// console.log(howBad(98765));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 13 - February - 2021
 // Source: https://edabit.com/challenge/LhBwyiMeWiADZhydE
 // Title: Learn Lodash (4): _.dropRight, Drop the Last Elements of an Array
