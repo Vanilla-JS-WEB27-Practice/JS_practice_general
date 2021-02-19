f133d6eda060878a1754f352b918fe5a71f57af7
seanaleid <seanaleid@gmail.com>
2021-02-18 23:41:58 -0500

    Formating BigInts

--- practice02.js
+++ practice02.js
@@ -19,6 +19,36 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 18 - February - 2021
+// Source: https://edabit.com/challenge/cXgmHb6aXESZBvW4b
+// Title: Formating BigInts
+
+// Instructions:
+// Cryptocurrencies often have a lot of decimals. For example, the popular cryptocurrency Ethereum has 18 decimals.
+// When dealing with money, precision is important, you don't want to lose money because a number is losing precision. 
+// However, with JavaScript, normal numbers only can go up to 9007199254740991. To deal with this, Javascript now has BigInt for integers bigger than that.
+// However, in order to get back to a decimal number, the number needs to be formatted from a BigInt to a string with the right number of decimals.
+// Write a function that takes as arguments a BigInt and the desired amount of decimals and returns a string (not a number, as it will lose precision) with the correct amount of decimals.
+
+// Examples:
+// formatBigInt(1938908490185852058934n, 18) ➞ "1938.908490185852058934"
+// formatBigInt(987654321987654321n, 6 ) ➞ "987654321987.654321"
+// formatBigInt(13902183984901849081284n, 12) ➞ "13902183984.901849081284"
+
+// Work:
+// const formatBigInt = (bn, d) => {
+//   const p2 = String(bn).slice(String(bn).length - d)
+//   const p1 = String(bn).slice(0,String(bn).length - d)
+//   return [p1,'.',p2].join("")
+// }
+
+// console.log(formatBigInt(1938908490185852058934n, 18));
+// console.log(formatBigInt(987654321987654321n, 6));
+// console.log(formatBigInt(13902183984901849081284n, 12));
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 17 - February - 2021
 // Source: https://edabit.com/challenge/Dq8kbbsLYyG9are5Z
 // Title: Total Volume of All Boxes
@@ -38,6 +68,11 @@
 // Each box will always have three dimensions included.
 
 // Work:
+// solution by Pustur, edabit user
+// const totalVolume = (...boxes) => boxes.reduce((total, [l,w,h]) => total + l * w * h, 0);
+
+
+// My solution 
 // const totalVolume = (...boxes) => {
 //   let result = 0;
 //   boxes.map(x => result = result + (x[0]*x[1]*x[2]));
