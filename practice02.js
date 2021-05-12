289fb7e7e5089398c05fe2223910d278e792d529
seanaleid <seanaleid@gmail.com>
2021-05-11 21:32:43 -0400

    Come Check Out This Crazy Function

--- practice02.js
+++ practice02.js
@@ -20,6 +20,27 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 11 - May - 2021
+// Source: https://edabit.com/challenge/Fp7p3BoHkF5Dub8pq
+// Title: Come Check Out This Crazy Function
+
+// Instructions:
+// You are given two numbers, a and b. Write a function which uses an expression to return the expected output. I am not going to tell you what the expression is because that will spoil the fun! :)
+
+// Examples:
+// crazyfunction(10, 20) ➞ 30
+// crazyfunction(17, 35) ➞ 50
+// crazyfunction(61, 233) ➞ 212
+
+// Work:
+// const crazyfunction = (a,b) => a^b
+
+// console.log(crazyfunction(10,20));
+// console.log(crazyfunction(17,35));
+// console.log(crazyfunction(61,233));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 11 - May - 2021
 // Source: https://edabit.com/challenge/4NKNkPZtN39cqCQMk
 // Title: String Expansion
 
