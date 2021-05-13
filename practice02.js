fa63759d72564bf5f9d9cabe13b308f65709c189
seanaleid <seanaleid@gmail.com>
2021-05-12 22:22:26 -0400

    Sum of Resistance in Series Circuits

--- practice02.js
+++ practice02.js
@@ -20,6 +20,37 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 12 - May - 2021
+// Source: https://edabit.com/challenge/JDkyQJqNfJNhvjmRW
+// Title: Sum of Resistance in Series Circuits
+
+// Instructions:
+// When resistors are connected together in series, the same current passes through each resistor in the chain and the total resistance, 
+// RT, of the circuit must be equal to the sum of all the individual resistors added together. That is:
+// RT = R1 + R2 + R3 ...
+// Create a function that takes an array of values resistance that are connected in series, and calculates the total resistance of the circuit in ohms. 
+// The ohm is the standard unit of electrical resistance in the International System of Units ( SI ).
+
+// Examples:
+// seriesResistance([1, 5, 6, 3]) ➞ "15 ohms"
+// seriesResistance([16, 3.5, 6]) ➞ "25.5 ohms"
+// seriesResistance([0.5, 0.5]) ➞ "1.0 ohm"
+
+// Notes:
+// All inputs will be valid.
+// Notice the singular ohm for values <= 1.
+// This challenge was inspired by Joshua Señoron's Python Sum of Resistance in Parallel Circuits challenge. You can find it here.
+// https://edabit.com/challenge/dCnX639Sheqdxqjm2
+
+// Work:
+// const seriesResistance = (arr) => arr.reduce((a,c) => a+c,0) <= 1 ? `${arr.reduce((a,c) => a+c,0)} ohm` : `${arr.reduce((a,c) => a+c,0)} ohms`;
+
+// console.log(seriesResistance([1,5,6,3]));
+// console.log(seriesResistance([16,3.5,6]));
+// console.log(seriesResistance([0.5,0.5]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 12 - May - 2021
 // Source: https://edabit.com/challenge/t5w3KeLXzs5ChWDMo
 // Title: Sharing is Caring
 
