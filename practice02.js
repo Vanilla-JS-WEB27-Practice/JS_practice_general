b0dffb7358e8cbc2cdc2d48c8d8c87038f164b2a
seanaleid <seanaleid@gmail.com>
2021-05-17 18:19:20 -0400

    Proper Modulo Operator

--- practice02.js
+++ practice02.js
@@ -19,6 +19,31 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 17 - May - 2021
+// Source: https://edabit.com/challenge/2eiEBoMFJZhE2SjHz
+// Title: Proper Modulo Operator
+
+// Instructions:
+// While useful, the modulo operator in JavaScript is not infallible when negative numbers are thrown into the mix. 
+// For example, -13 % 64 = -13, when the actual answer is 51. Create a function to fix this. It should also work for positive numbers.
+
+// Examples:
+// mod(-13, 64) ➞ 51
+// mod(50, 25) ➞ 0
+// mod(-6, 3) ➞ 0
+
+// Notes:
+// All test cases contain valid numbers.
+
+// Work:
+const mod(m,n) => ((m%n)+n)%n
+
+console.log(mod(-13, 63));
+console.log(mod(50, 25));
+console.log(mod(-6, 3));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 14 - May - 2021
 // Source: https://edabit.com/challenge/r4ZHGRs8uoK2HpEFo
 // Title: Sieve of Eratosthenes
