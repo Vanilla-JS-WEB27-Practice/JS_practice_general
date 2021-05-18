63b8ed46a5f02c5c872728e70e6f0eac9f69dded
seanaleid <seanaleid@gmail.com>
2021-05-17 21:30:45 -0400

    Radioactive Decay

--- practice02.js
+++ practice02.js
@@ -20,6 +20,57 @@
 // ------------------------------------------------------------------------------------------------------------------
 
 // Date: 17 - May - 2021
+// Source: https://edabit.com/challenge/bQPSi3s2zp6sjrYJL
+// Title: Radioactive Decay
+// Resources: https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Nuclear_Chemistry/Nuclear_Kinetics/Half-Lives_and_Radioactive_Decay_Kinetics
+// unary plus operator --> https://stackabuse.com/javascript-convert-string-to-number/ 
+// unary plus operator and other methods to cast strings into numbers --> https://medium.com/@nikjohn/cast-to-number-in-javascript-using-the-unary-operator-f4ca67c792ce 
+// mdn docs unary plus operator --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_plus
+// mdn docs unary minus operator --> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Unary_negation --> basically just a negative 
+
+// Instructions:
+// A half life is the amount of time for half of a radioactive substance to decay.
+// After 1 half life, 50% of a substance will be left.
+// After 2 half lives, 25% of a substance will be left.
+// After 3 half lives, 12.5% of a substance will be left, etc...
+
+// Create a function which calculates the remaining mass and the number of years that it took for the substance to decay. You will be given:
+// The mass of the substance.
+// The time in years for a halflife to elapse.
+// The number of half lives.
+
+// Worked Example
+// halflifeCalculator(1000, 5730, 2) ➞ [250, 11460]
+// There are 2 half lives, so the mass decays from 1000 to 500, then from 500 to 250.
+// Each halflife is 5730 years, and since there were 2, it took 11460 years in total.
+
+// Examples:
+// halflifeCalculator(1600, 6, 3) ➞ [200, 18]
+// halflifeCalculator(13, 500, 1) ➞ [6.5, 500]
+// halflifeCalculator(100, 35, 5) ➞ [3.125, 175]
+
+// Notes:
+// Round the final mass to three decimal places.
+// All inputs are positive numbers.
+// Return the final mass first, then the number of years.
+
+// Work:
+
+// solution from Radwa edabit user
+// const halflifeCalculator = (m, h, n) => [+(.5**n*m).toFixed(3),h*n];
+
+// first pass solution
+// const halflifeCalculator = (m, h, n) => [Number((.5**n*m).toFixed(3)),h*n];
+
+// console.log(halflifeCalculator(1600,6,3));
+// console.log(halflifeCalculator(13,500,1));
+// console.log(halflifeCalculator(100,35,5));
+// console.log(halflifeCalculator(11037,53,6));
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 17 - May - 2021
 // Source: https://edabit.com/challenge/7KbZc8QvzqrJPaE6Q
 // Title: RegEx XIV: Group Ranges x|y
 
