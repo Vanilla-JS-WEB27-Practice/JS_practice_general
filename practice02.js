e9b4a507aad6e22481420488d71977c2901dcc5d
seanaleid <seanaleid@gmail.com>
2021-05-19 23:14:39 -0400

    Finish the Sentence You're On

--- practice02.js
+++ practice02.js
@@ -19,15 +19,65 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 19 - May - 2021
+// Source: https://edabit.com/challenge/MarKs2qWR3cMHZjxy
+// Title: Finish the Sentence You're On!
+// Resource: REGEX example: https://stackoverflow.com/questions/5963182/how-to-remove-spaces-from-a-string-using-javascript 
+
+// Instructions:
+// POV: You are in an exam and time has just run out. While the teacher's back is turned, you hastily take the opportunity to finish scribbling down the last few words of the conclusion.
+
+// For this challenge, it takes 0.5 seconds to write a character (not including spaces). Given the full sentence and the unfinished sentence as inputs, return the time it takes to finish writing in seconds.
+
+// Worked Example
+// timeToFinish(
+//   "And so brings my conclusion to its conclusion.",
+//   "And so brings my conclusion to"
+// ) ➞ 7
+
+// "its" has 3 characters
+// "conclusion." has 11 characters, including punctuation.
+// 11 + 3 = 14
+// 14 x 0.5 = 7
+// Remember not to include spaces.
+
+// Examples:
+// timeToFinish(
+//   "And so brings my conclusion to its conclusion.",
+//   "And so brings my conclusion to its conclus"
+// ) ➞ 2
+
+// timeToFinish(
+//   "As a result, my point is still valid.",
+//   "As a result, my"
+// ) ➞ 9
+
+// timeToFinish(
+//   "Thank you for reading my essay.",
+//   "T"
+// ) ➞ 12.5
+
+// Notes:
+// The unfinished sentence is always found at the start of a complete sentence.
+
+// Work:
+// const timeToFinish = (s1, s2) => (s1.replace(/\s/g,"").length - s2.replace(/\s/g,"").length)*0.5;
+
+// console.log(timeToFinish("And so brings my conclusion to its conclusion.","And so brings my conclusion to its conclus"));
+// console.log(timeToFinish("As a result, my point is still valid.","As a result, my"));
+// console.log(timeToFinish("Thank you for reading my essay.","T"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // NOT FINISHED
 
 // Template
-// Date: 17 - May - 2021
+// Date: 18 - May - 2021
 // Source: https://edabit.com/challenge/bCif823bBvNFfLK7G
 // Title: Even or Odd: Which is Greater?
 
 // Instructions:
-// Create a function to determine if the sum of all the individual even digits are greater than the sum of all the indiviudal odd digits in a string of numbers.
+// Create a function to determine if the sum of all the individual even digits are greater than the sum of all the individual odd digits in a string of numbers.
 // If the sum of odd numbers is greater than the sum of even numbers, return "Odd is greater than Even".
 // If the sum of even numbers is greater than the odd numbers, return "Even is greater than Odd".
 // If the sum of both even and odd numbers are equal, return "Even and Odd are the same".
