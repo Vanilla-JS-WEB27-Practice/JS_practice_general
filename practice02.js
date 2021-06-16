f11de785adf4a6ddd5a1febac80417d064a4afdb
seanaleid <seanaleid@gmail.com>
2021-06-16 19:59:26 -0400

    Burglary Series (12): Get Vodka Bottle

--- practice02.js
+++ practice02.js
@@ -19,6 +19,46 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 16 - June - 2021
+// Source: https://edabit.com/challenge/paMpZoEJ6gr4feMS3
+// Title: Burglary Series (12): Get Vodka Bottle
+
+// Instructions:
+// The insurance guy laughs, he's just kidding. He just needs an updated list. You just need one of those Rammstein Vodka bottles.
+// Given an object with alcoholic drinks and a number, return a string with the name of the Rammstein bottle that matches the given number.
+
+// Examples:
+// { whiskey: 100, "Rammstein A": 100, "Rammstein B": 50 } ➞ "Rammstein A"
+// number = 100
+// { whiskey: 100, "Rammstein A": 100, "Rammstein B": 50 } ➞ "Rammstein B"
+// number = 50
+// { whiskey: 100, "Rammstein A": 100, "Rammstein D": 70, beer: 70 } ➞ "Rammstein D"
+// number = 70
+
+// Notes:
+// There will always be a corresponding Rammstein bottle for the number given.
+// There will never be 2 Rammstein bottles with the same number.
+// You always return one Rammstein bottle.
+
+// Work:
+// solution by Puster Edabit user
+// const getVodkaBottle = (obj, num) => Object.keys(obj).find(key => key.includes('Rammstein') && obj[key] === num);
+
+// first pass
+// const getVodkaBottle = (o,n) => {
+//   for(const alc in o){
+//     if (alc.includes("Rammstein") && o[alc] === n){
+//       return alc
+//     }
+//   }
+// }
+
+// console.log(getVodkaBottle({ whiskey: 100, "Rammstein A": 100, "Rammstein B": 50 }, 100));
+// console.log(getVodkaBottle({ whiskey: 100, "Rammstein A": 100, "Rammstein B": 50 }, 50));
+// console.log(getVodkaBottle({ whiskey: 100, "Rammstein A": 100, "Rammstein D": 70, beer: 70 }, 70));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 15 - June - 2021
 // Source: https://edabit.com/challenge/ahAX627M3Y26tij9y
 // Title: Pluralize!
@@ -57,9 +97,9 @@
 //   return  test
 // }
 
-console.log(pluralize(["cow", "pig", "cow", "cow"]));
-console.log(pluralize(["table", "table", "table"]));
-console.log(pluralize(["chair", "pencil", "arm"]));
+// console.log(pluralize(["cow", "pig", "cow", "cow"]));
+// console.log(pluralize(["table", "table", "table"]));
+// console.log(pluralize(["chair", "pencil", "arm"]));
 
 
 // ------------------------------------------------------------------------------------------------------------------
