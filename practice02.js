9228c5809b0ddc926ea747f548b58f8e0d95b815
seanaleid <seanaleid@gmail.com>
2021-06-25 23:07:26 -0400

    Word to Bitstring to Boolean Array

--- practice02.js
+++ practice02.js
@@ -19,6 +19,55 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 25 - June - 2021
+// Source: https://edabit.com/challenge/9JcSwa4dN5PEmdBEg
+// Title: Word to Bitstring to Boolean Array
+
+// Instructions:
+// Create a function that converts a word to a bitstring and then to a boolean array based on the following criteria:
+// Locate the position of the letter in the English alphabet (from 1 to 26).
+// Odd positions will be represented as 1 and even positions will be represented as 0.
+// Convert the represented positions to boolean values, 1 for true and 0 for false.
+// Store the conversions into an array.
+
+// Examples:
+// toBoolArray("deep") ➞ [false, true, true, false]
+// deep converts to 0110
+// d is the 4th alphabet - 0
+// e is the 5th alphabet - 1
+// e is the 5th alphabet - 1
+// p is the 16th alphabet - 0
+// toBoolArray("loves") ➞ [false, true, false, true, true]
+// toBoolArray("tesh") ➞ [false, true, true, false]
+
+// Notes:
+// The letter A is at position 1 and Z at 26.
+// All input strings are in lowercase letters of the English alphabet.
+
+// Work:
+// solution from afairwell2pawns edabit user
+// const toBoolArray = (w) => [...w].map(i => Boolean(i.charCodeAt() % 2));
+
+// third pass
+// const toBoolArray = (w) => w.split("").map(x => x.charCodeAt(0)-96 % 2 !== 0);
+
+// second pass
+// const toBoolArray = (w) => w.split("").map(x => x.charCodeAt(0)-96).map(x => x % 2 !== 0);
+
+// first pass 
+// const toBoolArray = (w) => {
+//   const splitWord = w.split("");
+//   const arr = splitWord.map(x => x.charCodeAt(0)-96).map(x => x % 2 !== 0)
+//   return arr;
+// }
+
+// console.log(toBoolArray("deep"));
+// console.log(toBoolArray("loves"));
+// console.log(toBoolArray("tesh"));
+// console.log(toBoolArray("abcdefghijklmnopqrstuvwxyz"));
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 23 - June - 2021
 // Source: https://edabit.com/challenge/JDDeK9jSFKJbfzhMt
 // Title: Same on Both Ends
