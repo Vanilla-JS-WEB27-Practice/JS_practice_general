0cb116859d81ba9098b4a5b0e0787425c4704afd
seanaleid <seanaleid@gmail.com>
2021-07-10 22:56:39 -0400

    Two Distint Elements

--- practice02.js
+++ practice02.js
@@ -19,7 +19,40 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
-// Date: - July - 2021
+// Date: 10 - July - 2021
+// Source: https://edabit.com/challenge/TyX7xfsAPfJZxfaZq
+// Title: Two Distinct Elements
+
+// Instructions:
+// In each input array, every number repeats at least once, except for two. Write a function that returns the two unique numbers.
+
+// Examples:
+// returnUnique([1, 9, 8, 8, 7, 6, 1, 6]) ➞ [9, 7]
+// returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]) ➞ [2, 1]
+// returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]) ➞ [5, 6]
+
+// Notes:
+// Keep the same ordering in the output.
+
+// Work:
+// modified solution from BlueAlps edabit user 
+// returnUnique = (a) => a.filter(x => a.indexOf(x) === a.lastIndexOf(x));
+
+
+// first pass
+// const returnUnique = (a) => {
+//   const result = [];
+//   a.map(x => a.indexOf(x) == a.lastIndexOf(x) ? result.push(x) : null);
+//   return result
+// }
+
+// console.log(returnUnique([1, 9, 8, 8, 7, 6, 1, 6]));
+// console.log(returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]));
+// console.log(returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]));
+
+// ------------------------------------------------------------------------------------------------------------------
+
+// Date: 06 - July - 2021
 // Source: https://edabit.com/challenge/BFJbFNJv2E8icogRk
 // Title: Secret Function
 
