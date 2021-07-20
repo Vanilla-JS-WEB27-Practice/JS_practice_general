cf2fe5da2a59d404c82917f1be989049ba9a50a7
seanaleid <seanaleid@gmail.com>
2021-07-20 00:02:26 -0400

    Perimeter with a catch

--- practice02.js
+++ practice02.js
@@ -19,6 +19,42 @@
 
 // ------------------------------------------------------------------------------------------------------------------
 
+// Date: 19 - July - 2021
+// Source: https://edabit.com/challenge/WEvqZTFcHeYzFn74c
+// Title: Perimeters with a Catch
+
+// Instructions:
+// Write a function that takes a number and returns the perimeter of either a circle or a square. The input will be in the form (letter l, number num) where the letter will be either "s" for square, or "c" for circle, and the number will be the side of the square or the radius of the circle.
+
+// Use the following formulas:
+
+// Perimeter of a square: 4 * side.
+// Perimeter of a circle: 6.28 * radius.
+// The catch is you can only use arithmetic or comparison operators, which means:
+
+// No if... else statements.
+// No objects.
+// No arrays.
+// No formatting methods, etc.
+// The goal is to write a short, branch-free piece of code.
+
+// Examples:
+// perimeter("s", 7) ➞ 28
+
+// perimeter("c", 4) ➞ 25.12
+
+// perimeter("c", 9) ➞ 56.52
+
+// Work:
+// const perimeter = (l, num) => l === "c" ? 6.28 * num : 4 * num;
+
+// console.log(perimeter("s", 7));
+// console.log(perimeter("c", 4));
+// console.log(perimeter("c", 9));
+
+
+// ------------------------------------------------------------------------------------------------------------------
+
 // Date: 15 - July - 2021
 // Source: https://edabit.com/challenge/XdAR3KohR5w7rjrFg
 // Title: Sum of Cubes
@@ -648,7 +684,7 @@
 // Work:
 
 // second pass
-const shiftToLeft = (x,y) => y <= 0 ? x : shiftToLeft(x *= 2,y-1)
+// const shiftToLeft = (x,y) => y <= 0 ? x : shiftToLeft(x *= 2,y-1)
 
 // first pass
 // const shiftToLeft = (x,y) => {
@@ -671,13 +707,13 @@ const shiftToLeft = (x,y) => y <= 0 ? x : shiftToLeft(x *= 2,y-1)
 //   return parseInt(zero, 2);
 // }
 
-console.log(shiftToLeft(5,1));
-console.log(shiftToLeft(5,2));
-console.log(shiftToLeft(10,3));
-console.log(shiftToLeft(-32,2));
-console.log(shiftToLeft(-6,5));
-console.log(shiftToLeft(12,4));
-console.log(shiftToLeft(46,6));
+// console.log(shiftToLeft(5,1));
+// console.log(shiftToLeft(5,2));
+// console.log(shiftToLeft(10,3));
+// console.log(shiftToLeft(-32,2));
+// console.log(shiftToLeft(-6,5));
+// console.log(shiftToLeft(12,4));
+// console.log(shiftToLeft(46,6));
 
 // ------------------------------------------------------------------------------------------------------------------
 
